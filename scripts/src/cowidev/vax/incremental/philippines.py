9cedb3eb19e20bf4a07d716ce966a2c1de35720e
Edouard Mathieu <edmat@pm.me>
2021-08-30 14:58:12 +0200

    fix(vax,philippines): update vaccine list

--- scripts/src/cowidev/vax/incremental/philippines.py
+++ scripts/src/cowidev/vax/incremental/philippines.py
@@ -52,7 +52,7 @@ class Philippines:
         return enrich_data(
             ds,
             "vaccine",
-            "Johnson&Johnson, Moderna, Oxford/AstraZeneca, Pfizer/BioNTech, Sinovac, Sputnik V",
+            "Covaxin, Johnson&Johnson, Moderna, Novavax, Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing, Sinovac, Sputnik V",
         )
 
     def pipe_source(self, ds: pd.Series) -> pd.Series:
