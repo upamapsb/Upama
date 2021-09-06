7c7518b80549b4909e6971eb386239e69ec031ac
lucas rg <lucasrg@kth.se>
2021-09-06 17:44:01 +0200

    enhance(vax): organise orgs imports

--- scripts/src/cowidev/vax/utils/orgs/__init__.py
+++ scripts/src/cowidev/vax/utils/orgs/__init__.py
@@ -1,20 +1,21 @@
 import os
 
-from ._config_loader import vaccines_mapping, countries_mapping
+from ._config_loader import get_org_constants
 
 __CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # WHO
 __WHO_CONFIG = os.path.join(__CURRENT_DIR, "who_config.yaml")
-WHO_VACCINES = vaccines_mapping(__WHO_CONFIG)
-WHO_COUNTRIES = countries_mapping(__WHO_CONFIG)
+WHO_COUNTRIES, WHO_VACCINES = get_org_constants(__WHO_CONFIG)
 
 # PAHO
 __PAHO_CONFIG = os.path.join(__CURRENT_DIR, "paho_config.yaml")
-PAHO_VACCINES = vaccines_mapping(__PAHO_CONFIG)
-PAHO_COUNTRIES = countries_mapping(__PAHO_CONFIG)
+PAHO_COUNTRIES, PAHO_VACCINES = get_org_constants(__PAHO_CONFIG)
 
 # AFRICACDC
 __AFRICACDC_CONFIG = os.path.join(__CURRENT_DIR, "acdc_config.yaml")
-ACDC_VACCINES = vaccines_mapping(__AFRICACDC_CONFIG)
-ACDC_COUNTRIES = countries_mapping(__AFRICACDC_CONFIG)
+ACDC_COUNTRIES, ACDC_VACCINES = get_org_constants(__AFRICACDC_CONFIG)
+
+# SPC
+__SPC_CONFIG = os.path.join(__CURRENT_DIR, "spc_config.yaml")
+SPC_COUNTRIES, SPC_VACCINES = get_org_constants(__SPC_CONFIG)
