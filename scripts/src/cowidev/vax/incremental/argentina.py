242a5ac4bcdafdf2ba146e25ea933d972fb58677
lucas rg <lucasrg@kth.se>
2021-09-09 20:38:05 +0200

    fix(vax,aregntina): refactor + new vaccine

--- scripts/src/cowidev/vax/incremental/argentina.py
+++ scripts/src/cowidev/vax/incremental/argentina.py
@@ -4,90 +4,95 @@ from cowidev.vax.utils.incremental import enrich_data, increment
 from cowidev.utils.clean.dates import localdate
 
 
-def read(source: str) -> pd.Series:
-    df = pd.read_csv(
-        source,
-        usecols=[
-            "primera_dosis_cantidad",
-            "segunda_dosis_cantidad",
-            "vacuna_nombre",
-        ],
+class Argentina:
+    location: str = "Argentina"
+    source_url_ref: str = (
+        "http://datos.salud.gob.ar/dataset/vacunas-contra-covid-19-dosis-aplicadas-en-la-republica-argentina"
     )
+    source_url: str = "https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19VacunasAgrupadas.csv.zip"
+    vaccine_mapping: dict = {
+        "COVISHIELD ChAdOx1nCoV COVID 19": "Oxford/AstraZeneca",
+        "AstraZeneca ChAdOx1 S recombinante": "Oxford/AstraZeneca",
+        "Sinopharm Vacuna SARSCOV 2 inactivada": "Sinopharm/Beijing",
+        "Sputnik V COVID19 Instituto Gamaleya": "Sputnik V",
+        "Moderna ARNm": "Moderna",
+        "Cansino Ad5 nCoV": "CanSino",
+    }
+    one_dose_vaccine: list = ["Cansino Ad5 nCoV"]
 
-    known_vaccines = set(
-        (
-            "AstraZeneca ChAdOx1 S recombinante",
-            "COVISHIELD ChAdOx1nCoV COVID 19",
-            "Sinopharm Vacuna SARSCOV 2 inactivada",
-            "Sputnik V COVID19 Instituto Gamaleya",
-            "Moderna ARNm",
+    def read(self) -> pd.DataFrame:
+        return pd.read_csv(
+            self.source_url,
+            usecols=[
+                "primera_dosis_cantidad",
+                "segunda_dosis_cantidad",
+                "vacuna_nombre",
+            ],
         )
-    )
-    assert set(df.vacuna_nombre) == known_vaccines, "New vaccine found!"
 
-    return df.drop(columns="vacuna_nombre").sum()
+    def pipe_vaccine_checks(self, df: pd.DataFrame) -> pd.DataFrame:
+        vaccines_unknown = set(df.vacuna_nombre).difference(self.vaccine_mapping)
+        if vaccines_unknown:
+            raise ValueError(f"Unknown vaccine name(s): {vaccines_unknown}")
+        return df
 
+    def pipe_metrics(self, df: pd.DataFrame) -> pd.Series:
+        mask = df.vacuna_nombre.isin(self.one_dose_vaccine)
+        df_1d = df.loc[mask]
+        df_nd = df.loc[~mask]
+        single_shots = df_1d["primera_dosis_cantidad"].sum()
+        first_shots = df_nd["primera_dosis_cantidad"].sum()
+        second_shots = df_nd["segunda_dosis_cantidad"].sum()
 
-def translate_index(ds: pd.Series) -> pd.Series:
-    return ds.rename(
-        {
-            "primera_dosis_cantidad": "people_vaccinated",
-            "segunda_dosis_cantidad": "people_fully_vaccinated",
-        }
-    )
+        if df_1d["segunda_dosis_cantidad"].sum() != 0:
+            raise ValueError(
+                f"`segunda_dosis_cantidad` field for one dose vaccines ({self.one_dose_vaccine}) must be 0!"
+            )
+        return pd.Series(
+            {
+                "people_vaccinated": first_shots + single_shots,
+                "people_fully_vaccinated": second_shots + single_shots,
+                "total_vaccinations": df["primera_dosis_cantidad"].sum() + df["segunda_dosis_cantidad"].sum(),
+                "vaccine": set(df.vacuna_nombre.replace(self.vaccine_mapping)),
+            }
+        )
 
+    def pipe_date(self, ds: pd.Series) -> pd.Series:
+        date = localdate("America/Argentina/Buenos_Aires", 8)
+        return enrich_data(ds, "date", date)
 
-def add_totals(ds: pd.Series) -> pd.Series:
-    total_vaccinations = ds["people_vaccinated"] + ds["people_fully_vaccinated"]
-    return enrich_data(ds, "total_vaccinations", total_vaccinations)
+    def pipe_location(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "location", self.location)
 
+    def pipe_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(
+            ds,
+            "source_url",
+            self.source_url_ref,
+        )
 
-def format_date(ds: pd.Series) -> pd.Series:
-    date = localdate("America/Argentina/Buenos_Aires", 8)
-    return enrich_data(ds, "date", date)
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return (
+            ds.pipe(self.pipe_vaccine_checks)
+            .pipe(self.pipe_metrics)
+            .pipe(self.pipe_date)
+            .pipe(self.pipe_location)
+            .pipe(self.pipe_source)
+        )
 
-
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Argentina")
-
-
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Moderna, Oxford/AstraZeneca, Sinopharm/Beijing, Sputnik V")
-
-
-def enrich_source(ds: pd.Series) -> pd.Series:
-    return enrich_data(
-        ds,
-        "source_url",
-        "http://datos.salud.gob.ar/dataset/vacunas-contra-covid-19-dosis-aplicadas-en-la-republica-argentina",
-    )
-
-
-def pipeline(ds: pd.Series) -> pd.Series:
-    return (
-        ds.pipe(translate_index)
-        .pipe(add_totals)
-        .pipe(format_date)
-        .pipe(enrich_location)
-        .pipe(enrich_vaccine)
-        .pipe(enrich_source)
-    )
+    def export(self, paths):
+        data = self.read().pipe(self.pipeline)
+        increment(
+            paths=paths,
+            location=str(data["location"]),
+            total_vaccinations=int(data["total_vaccinations"]),
+            people_vaccinated=int(data["people_vaccinated"]),
+            people_fully_vaccinated=int(data["people_fully_vaccinated"]),
+            date=str(data["date"]),
+            source_url=str(data["source_url"]),
+            vaccine=str(data["vaccine"]),
+        )
 
 
 def main(paths):
-    source = "https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19VacunasAgrupadas.csv.zip"
-    data = read(source).pipe(pipeline)
-    increment(
-        paths=paths,
-        location=str(data["location"]),
-        total_vaccinations=int(data["total_vaccinations"]),
-        people_vaccinated=int(data["people_vaccinated"]),
-        people_fully_vaccinated=int(data["people_fully_vaccinated"]),
-        date=str(data["date"]),
-        source_url=str(data["source_url"]),
-        vaccine=str(data["vaccine"]),
-    )
-
-
-if __name__ == "__main__":
-    main()
+    Argentina().export(paths)
