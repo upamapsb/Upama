8c69128f8197045ecbe10d634d9c4dd6a66194c3
Edouard Mathieu <edmat@pm.me>
2021-09-09 14:48:06 +0200

    enhance: use CET for update alerts

--- scripts/scripts/update_alerts.sh
+++ scripts/scripts/update_alerts.sh
@@ -3,16 +3,16 @@ set -e
 cd /home/owid/covid-19-data/scripts
 source venv/bin/activate
 
-hour=$(date +%H)
+hour=$(TZ=Europe/Paris date +%H)
 
 if [ $hour == 7 ] ; then
   python scripts/update_alerts.py --data jhu
 fi
 
-if [ $hour == 11 ] ; then
+if [ $hour == 12 ] ; then
   python scripts/update_alerts.py --data vax
 fi
 
-if [ $hour == 15 ] ; then
+if [ $hour == 14 ] ; then
   python scripts/update_alerts.py --data testing
 fi
