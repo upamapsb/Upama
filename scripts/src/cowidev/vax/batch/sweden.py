f07cda17a949f49643235632dca7b9249ad04774
lucas rg <lucasrg@kth.se>
2021-09-09 21:03:14 +0200

    fix(vax,sweden): change in source data format #1878

--- scripts/src/cowidev/vax/batch/sweden.py
+++ scripts/src/cowidev/vax/batch/sweden.py
@@ -2,16 +2,17 @@ import datetime
 
 import pandas as pd
 
-from cowidev.vax.utils.utils import make_monotonic
 from cowidev.utils.clean import clean_count
+from cowidev.utils.web.scraping import get_soup
+from cowidev.vax.utils.utils import make_monotonic
 
 
 class Sweden(object):
     def __init__(self):
         """Constructor."""
         self.source_url_daily = (
-            "https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/"
-            "vaccination-mot-covid-19/statistik/statistik-over-registrerade-vaccinationer-covid-19/"
+            "https://fohm.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/statistik-och-analyser/"
+            "statistik-over-registrerade-vaccinationer-covid-19/"
         )
         self.source_url_weekly = (
             "https://fohm.maps.arcgis.com/sharing/rest/content/items/fc749115877443d29c2a49ea9eca77e9/data"
@@ -61,34 +62,60 @@ class Sweden(object):
 
     def _read_daily_data(self) -> pd.DataFrame:
         dfs = pd.read_html(self.source_url_daily, encoding="utf-8")
-        df_adults = self._get_df_adults_daily(dfs)
-        df_teens = self._get_df_teens_daily(dfs)
-        df_doses = self._get_df_doses_daily(dfs)
-        df = self._merge_tables_daily(df_adults, df_teens, df_doses)
+        df = self._get_df_daily(dfs[1])
+        df_doses = self._get_df_doses_daily(dfs[0])
+        df = self._merge_tables_daily(df, df_doses)
         return df
 
-    def _get_df_adults_daily(self, dfs):
-        # People vaccinated >18 yo
-        return dfs[1].assign(
-            people_vaccinated=dfs[1]["Antal vaccinerademed minst 1 dos*"].apply(clean_count),
-            people_fully_vaccinated=dfs[1]["Antal vaccinerademed 2 doser"].apply(clean_count),
+    def _read_daily_data_split(self) -> pd.DataFrame:
+        dfs = pd.read_html(self.source_url_daily, encoding="utf-8")
+        df_adults = self._get_df_adults_daily(dfs[1])
+        df_teens = self._get_df_teens_daily(dfs[2])
+        df_doses = self._get_df_doses_daily(dfs[0])
+        df = self._merge_tables_daily_split(df_adults, df_teens, df_doses)
+        return df
+
+    def _get_df_daily(self, df):
+        return df.assign(
+            people_vaccinated=df["Antal vaccinerademed minst 1 dos*"].apply(clean_count),
+            people_fully_vaccinated=df["Antal vaccinerademed 2 doser"].apply(clean_count),
         )
 
-    def _get_df_teens_daily(self, dfs):
+    def _get_df_adults_daily(self, df):
+        # People vaccinated >18 yo
+        return df.assign(
+            people_vaccinated=df["Antal vaccinerademed minst 1 dos*"].apply(clean_count),
+            people_fully_vaccinated=df["Antal vaccinerademed 2 doser"].apply(clean_count),
+        )
+
+    def _get_df_teens_daily(self, df):
         # People vaccinated < 18 yo
-        df_teens = dfs[2].pivot("Datum", "Status", "Antal vaccinerade födda 2003-2005").reset_index()
+        df_teens = df.pivot("Datum", "Status", "Antal vaccinerade födda 2003-2005").reset_index()
         return df_teens.assign(
             people_vaccinated=df_teens["Minst 1 dos"].apply(clean_count),
             people_fully_vaccinated=df_teens["2 doser"].apply(clean_count),
         )
 
-    def _get_df_doses_daily(self, dfs):
+    def _get_df_doses_daily(self, df):
         # Total vaccinations
-        return dfs[0].assign(
-            total_vaccinations=dfs[0]["Antal vaccinationer"].apply(clean_count),
+        return df.assign(
+            total_vaccinations=df["Antal vaccinationer"].apply(clean_count),
         )
 
-    def _merge_tables_daily(self, df_adults, df_teens, df_doses):
+    def _merge_tables_daily(self, df, df_doses):
+        # Merge
+        df = df.merge(df_doses, on="Datum").rename(
+            columns={
+                "Datum": "date",
+            }
+        )
+        df[["people_vaccinated", "people_fully_vaccinated"]] = df[
+            ["people_vaccinated", "people_fully_vaccinated"]
+        ].astype("Int64")
+        df = df[["date", "total_vaccinations", "people_vaccinated", "people_fully_vaccinated"]]
+        return df
+
+    def _merge_tables_daily_split(self, df_adults, df_teens, df_doses):
         # Merge people metrics
         df = df_adults.merge(df_teens, on="Datum", how="left")
         df = df.assign(
