9800100bcc600bd7129db1bf4d817a279c926107
lucas rg <lucasrg@kth.se>
2021-09-22 22:09:46 +0200

    fix(vax,vietnam): regex, typo in source

--- scripts/src/cowidev/vax/incremental/vietnam.py
+++ scripts/src/cowidev/vax/incremental/vietnam.py
@@ -16,7 +16,7 @@ class Vietnam:
         "date": r"Bản tin dịch COVID-19 ngày (\d{1,2}/\d{1,2}) của Bộ",
         "metrics": (
             r"Trong ngày \d{1,2}/\d{1,2} có [\.\d]+ liều (?:vắc xin phòng|vaccine) COVID-19 được tiêm(?:.*)?. Như "
-            r"vậy, tổng số liều (?:vắc xin|vaccine) đã được tiêm là ([\d\.]+) liều"
+            r"vậy, tổng số liều (?:vắc xin|vaccine|vaccien) đã được tiêm là ([\d\.]+) liều"
             r", trong đó tiêm 1 mũi là ([\d\.]+) liều, tiêm mũi 2 là ([\d\.]+) liều"
         ),
     }
