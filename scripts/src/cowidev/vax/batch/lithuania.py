f9f0defaa6e71439290c35e4c7ac86a8f33b8294
lucas rg <lucasrg@kth.se>
2021-09-22 12:15:23 +0200

    refactor(vax,lithuania): object oriented

--- scripts/src/cowidev/vax/batch/lithuania.py
+++ scripts/src/cowidev/vax/batch/lithuania.py
@@ -6,13 +6,14 @@ import pandas as pd
 from cowidev.vax.utils.files import export_metadata
 
 
-def main(paths):
-
-    DATA_URL = (
+class Lithuania:
+    location: str = "Lithuania"
+    source_url: str = (
         "https://services3.arcgis.com/MF53hRPmwfLccHCj/arcgis/rest/services/"
         "covid_vaccinations_by_drug_name_new/FeatureServer/0/query"
     )
-    PARAMS = {
+    source_url_ref: str = ""
+    query_params: dict = {
         "f": "json",
         "where": "municipality_code='00'",
         "returnGeometry": False,
@@ -22,92 +23,109 @@ def main(paths):
         "resultRecordCount": 32000,
         "resultType": "standard",
     }
-    res = requests.get(DATA_URL, params=PARAMS)
 
-    data = [elem["attributes"] for elem in json.loads(res.content)["features"]]
+    def read(self):
+        res = requests.get(self.source_url, params=self.query_params)
+        if res.ok:
+            data = [elem["attributes"] for elem in json.loads(res.content)["features"]]
+            return pd.DataFrame.from_records(data)
+        raise ValueError("Source not valid/available!")
 
-    df = pd.DataFrame.from_records(data)
+    def pipe_date(self, df: pd.DataFrame) -> pd.DataFrame:
+        df["date"] = pd.to_datetime(df["date"], unit="ms")
+        # Correction for vaccinations wrongly attributed to early December 2020
+        df.loc[df.date < "2020-12-27", "date"] = pd.to_datetime("2020-12-27")
+        return df
 
-    df["date"] = pd.to_datetime(df["date"], unit="ms")
+    def pipe_reshape(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df[(df.vaccination_state != "Dalinai") & (df.vaccinated_cum > 0)].copy()
+        df.loc[df.vaccination_state == "Visi", "dose_number"] = 1
+        df.loc[df.vaccination_state == "Pilnai", "dose_number"] = 2
+        return df.drop(columns="vaccination_state")
 
-    # Correction for vaccinations wrongly attributed to early December 2020
-    df.loc[df.date < "2020-12-27", "date"] = pd.to_datetime("2020-12-27")
+    def pipe_vaccine(self, df: pd.DataFrame) -> pd.DataFrame:
+        vaccine_mapping = {
+            "Pfizer-BioNTech": "Pfizer/BioNTech",
+            "Moderna": "Moderna",
+            "AstraZeneca": "Oxford/AstraZeneca",
+            "Johnson & Johnson": "Johnson&Johnson",
+        }
+        vaccines_wrong = set(df["vaccine_name"].unique()).difference(vaccine_mapping)
+        if vaccines_wrong:
+            raise ValueError(f"Missing vaccines: {vaccines_wrong}")
+        df = df.replace(vaccine_mapping)
+        return df
 
-    # Reshape data
-    df = df[(df.vaccination_state != "Dalinai") & (df.vaccinated_cum > 0)].copy()
-    df.loc[df.vaccination_state == "Visi", "dose_number"] = 1
-    df.loc[df.vaccination_state == "Pilnai", "dose_number"] = 2
-    df = df.drop(columns="vaccination_state")
+    def pipeline_base(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.pipe(self.pipe_date).pipe(self.pipe_reshape).pipe(self.pipe_vaccine)
 
-    # Data by vaccine
-    vaccine_mapping = {
-        "Pfizer-BioNTech": "Pfizer/BioNTech",
-        "Moderna": "Moderna",
-        "AstraZeneca": "Oxford/AstraZeneca",
-        "Johnson & Johnson": "Johnson&Johnson",
-    }
-    vaccines_wrong = set(df["vaccine_name"].unique()).difference(vaccine_mapping)
-    if vaccines_wrong:
-        raise ValueError(f"Missing vaccines: {vaccines_wrong}")
-    # assert set(df["vaccine_name"].unique()) == set(vaccine_mapping.keys())
-    df = df.replace(vaccine_mapping)
-    vax = (
-        df.groupby(["date", "vaccine_name"], as_index=False)["vaccinated_cum"]
-        .sum()
-        .sort_values("date")
-        .rename(
-            columns={"vaccine_name": "vaccine", "vaccinated_cum": "total_vaccinations"}
+    def pipeline_manufacturer(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = (
+            df.groupby(["date", "vaccine_name"], as_index=False)["vaccinated_cum"]
+            .sum()
+            .sort_values("date")
+            .rename(columns={"vaccine_name": "vaccine", "vaccinated_cum": "total_vaccinations"})
         )
-    )
-    vax["location"] = "Lithuania"
-    vax.to_csv(paths.tmp_vax_out_man("Lithuania"), index=False)
-    export_metadata(vax, "Ministry of Health", DATA_URL, paths.tmp_vax_metadata_man)
+        df["location"] = self.location
+        return df
 
-    # Unpivot
-    df = (
-        df.groupby(["date", "dose_number", "vaccine_name"], as_index=False)
-        .sum()
-        .pivot(
-            index=["date", "vaccine_name"],
-            columns="dose_number",
-            values="vaccinated_cum",
+    def pipe_unpivot(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df.groupby(["date", "dose_number", "vaccine_name"], as_index=False)
+            .sum()
+            .pivot(
+                index=["date", "vaccine_name"],
+                columns="dose_number",
+                values="vaccinated_cum",
+            )
+            .fillna(0)
+            .reset_index()
+            .rename(columns={1: "people_vaccinated", 2: "people_fully_vaccinated"})
+            .sort_values("date")
         )
-        .fillna(0)
-        .reset_index()
-        .rename(columns={1: "people_vaccinated", 2: "people_fully_vaccinated"})
-        .sort_values("date")
-    )
 
-    # Total vaccinations
-    df = df.assign(total_vaccinations=df.people_vaccinated + df.people_fully_vaccinated)
+    def pipe_metrics(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.assign(total_vaccinations=df.people_vaccinated + df.people_fully_vaccinated)
+        # Single shot
+        msk = df.vaccine_name == "Johnson & Johnson"
+        df.loc[msk, "people_fully_vaccinated"] = df.loc[msk, "people_vaccinated"]
+        return df
 
-    # Single shot
-    msk = df.vaccine_name == "Johnson & Johnson"
-    df.loc[msk, "people_fully_vaccinated"] = df.loc[msk, "people_vaccinated"]
-
-    # Group by date
-    df = (
-        df.groupby("date")
-        .agg(
-            {
-                "people_fully_vaccinated": sum,
-                "people_vaccinated": sum,
-                "total_vaccinations": sum,
-                "vaccine_name": lambda x: ", ".join(sorted(x)),
-            }
+    def pipe_aggregate(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df.groupby("date")
+            .agg(
+                {
+                    "people_fully_vaccinated": sum,
+                    "people_vaccinated": sum,
+                    "total_vaccinations": sum,
+                    "vaccine_name": lambda x: ", ".join(sorted(x)),
+                }
+            )
+            .rename(columns={"vaccine_name": "vaccine"})
+            .reset_index()
+            .replace(0, pd.NA)
         )
-        .rename(columns={"vaccine_name": "vaccine"})
-        .reset_index()
-    )
-    df = df.replace(0, pd.NA)
 
-    df.loc[:, "location"] = "Lithuania"
-    df.loc[
-        :, "source_url"
-    ] = "https://experience.arcgis.com/experience/cab84dcfe0464c2a8050a78f817924ca/page/page_3/"
+    def pipe_metadata(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.assign(
+            location=self.location,
+            source_url=self.source_url_ref,
+        )
 
-    df.to_csv(paths.tmp_vax_out("Lithuania"), index=False)
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.pipe(self.pipe_unpivot).pipe(self.pipe_metrics).pipe(self.pipe_aggregate).pipe(self.pipe_metadata)
+
+    def export(self, paths):
+        df_base = self.read().pipe(self.pipeline_base)
+        # Manufacturer
+        df_man = df_base.pipe(self.pipeline_manufacturer)
+        df_man.to_csv(paths.tmp_vax_out_man("Lithuania"), index=False)
+        export_metadata(df_man, "Ministry of Health", self.source_url_ref, paths.tmp_vax_metadata_man)
+        # Main
+        df = df_base.pipe(self.pipeline)
+        df.to_csv(paths.tmp_vax_out(self.location), index=False)
 
 
-if __name__ == "__main__":
-    main()
+def main(paths):
+    Lithuania().export(paths)
