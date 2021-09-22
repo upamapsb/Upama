dac0f52360d95e885dd72a2131bb63e707234ba1
lucas rg <lucasrg@kth.se>
2021-09-22 16:08:37 +0200

    fix(vax,greenland): date regex

--- scripts/src/cowidev/vax/incremental/greenland.py
+++ scripts/src/cowidev/vax/incremental/greenland.py
@@ -2,61 +2,67 @@ import datetime
 
 import pandas as pd
 
-from cowidev.utils.clean import clean_count, clean_date
+from cowidev.utils.clean import clean_count, extract_clean_date
 from cowidev.utils.web.scraping import get_soup
 from cowidev.vax.utils.incremental import enrich_data, increment
 
 
-def read(source: str) -> pd.Series:
-    soup = get_soup(source)
+class Greenland:
+    location: str = "Greenland"
+    source_url: str = "https://corona.nun.gl"
+    regex = {"date": r".*Nutarterneqarpoq: (\d+. [a-zA-Z]+202\d)"}
 
-    counters = soup.find_all(class_="text-brand-blue")
-    dose_1 = clean_count(counters[1].text)
-    dose_2 = clean_count(counters[2].text)
-    assert dose_1 >= dose_2
+    def read(self) -> pd.Series:
+        soup = get_soup(self.source_url)
+        data = self._parse_data(soup)
+        return pd.Series(data=data)
 
-    date = soup.find(class_="text-gray-500").text
-    date = date + str(datetime.date.today().year)
-    date = clean_date(date, fmt="Nutarterneqarpoq: %d. %B%Y", lang="en")
+    def _parse_data(self, soup) -> dict:
+        return {**self._parse_data_metrics(soup), **self._parse_data_date(soup)}
 
-    return pd.Series({"people_vaccinated": dose_1, "people_fully_vaccinated": dose_2, "date": date})
+    def _parse_data_metrics(self, soup) -> dict:
+        counters = soup.find_all(class_="text-brand-blue")
+        dose_1 = clean_count(counters[1].text)
+        dose_2 = clean_count(counters[2].text)
+        if dose_1 < dose_2:
+            raise ValueError("dose_1 cannot be higher than dose_2")
+        return {"people_vaccinated": dose_1, "people_fully_vaccinated": dose_2}
 
+    def _parse_data_date(self, soup) -> dict:
+        date = soup.find(class_="text-gray-500").text
+        date = date.strip() + str(datetime.date.today().year)
+        date = extract_clean_date(date, self.regex["date"], "%d. %B%Y")
+        return {"date": date}
 
-def enrich_source(ds: pd.Series, source: str) -> pd.Series:
-    return enrich_data(ds, "source_url", source)
+    def pipe_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "source_url", self.source_url)
 
+    def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "vaccine", "Moderna")
 
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Moderna")
+    def pipe_location(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "location", self.location)
 
+    def pipe_metrics(self, ds: pd.Series) -> pd.Series:
+        total_vaccinations = ds["people_vaccinated"] + ds["people_fully_vaccinated"]
+        return enrich_data(ds, "total_vaccinations", total_vaccinations)
 
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Greenland")
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return ds.pipe(self.pipe_location).pipe(self.pipe_vaccine).pipe(self.pipe_source).pipe(self.pipe_metrics)
 
-
-def add_totals(ds: pd.Series) -> pd.Series:
-    total_vaccinations = ds["people_vaccinated"] + ds["people_fully_vaccinated"]
-    return enrich_data(ds, "total_vaccinations", total_vaccinations)
-
-
-def pipeline(ds: pd.Series, source: str) -> pd.Series:
-    return ds.pipe(enrich_location).pipe(enrich_vaccine).pipe(enrich_source, source).pipe(add_totals)
+    def export(self, paths):
+        data = self.read().pipe(self.pipeline)
+        increment(
+            paths=paths,
+            location=data["location"],
+            total_vaccinations=data["total_vaccinations"],
+            people_vaccinated=data["people_vaccinated"],
+            people_fully_vaccinated=data["people_fully_vaccinated"],
+            date=data["date"],
+            source_url=data["source_url"],
+            vaccine=data["vaccine"],
+        )
 
 
 def main(paths):
-    source = "https://corona.nun.gl"
-    data = read(source).pipe(pipeline, source)
-    increment(
-        paths=paths,
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"],
-    )
-
-
-if __name__ == "__main__":
-    main()
+    Greenland().export(paths)
