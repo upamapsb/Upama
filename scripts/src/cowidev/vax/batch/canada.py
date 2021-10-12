21349c674958dd8471986acf771382b35a20ec46
Edouard Mathieu <edmat@pm.me>
2021-10-12 05:24:22 +0200

    enhance(vax,canada): make monotonic

--- scripts/src/cowidev/vax/batch/canada.py
+++ scripts/src/cowidev/vax/batch/canada.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from cowidev.utils.web import request_json
+from cowidev.vax.utils.utils import make_monotonic
 
 
 class Canada:
@@ -57,6 +58,7 @@ class Canada:
             .pipe(self.pipe_people_vaccinated)
             .pipe(self.pipe_metadata)
             .pipe(self.pipe_total_boosters)
+            .pipe(make_monotonic)
             .sort_values("date")
         )
         return df
