51c9ce9d4f33098bafc829a418a9f04050559e1d
Edouard Mathieu <edmat@pm.me>
2021-10-25 10:42:03 +0100

    enhance(vax,brazil): collect boosters

--- scripts/src/cowidev/vax/incremental/brazil.py
+++ scripts/src/cowidev/vax/incremental/brazil.py
@@ -31,21 +31,25 @@ class Brazil:
             for block in data_blocks:
                 block_title = block.find_element_by_class_name("sn-kpi-measure-title").get_attribute("title")
                 if "Total de Doses Aplicadas (Dose1)" in block_title:
-                    people_vaccinated = block.find_element_by_class_name("sn-kpi-value").text
+                    first_doses = clean_count(block.find_element_by_class_name("sn-kpi-value").text)
                 elif "Total de Doses Aplicadas (Doses 2 e Única)" in block_title:
-                    people_fully_vaccinated = block.find_element_by_class_name("sn-kpi-value").text
+                    people_fully_vaccinated = clean_count(block.find_element_by_class_name("sn-kpi-value").text)
                 elif "Total de Doses Aplicadas" in block_title:
-                    total_vaccinations = block.find_element_by_class_name("sn-kpi-value").text
-            doses_unique = self._parse_unique_doses(driver)
+                    total_vaccinations = clean_count(block.find_element_by_class_name("sn-kpi-value").text)
+                elif "Total Doses Adicionais" in block_title:
+                    additional_doses = clean_count(block.find_element_by_class_name("sn-kpi-value").text)
+                elif "Total Doses de Reforço" in block_title:
+                    booster_doses = clean_count(block.find_element_by_class_name("sn-kpi-value").text)
+            unique_doses = self._parse_unique_doses(driver)
 
         ds = pd.Series(
             {
                 "total_vaccinations": total_vaccinations,
-                "people_vaccinated": people_vaccinated,
+                "people_vaccinated": first_doses + unique_doses,
                 "people_fully_vaccinated": people_fully_vaccinated,
+                "total_boosters": additional_doses + booster_doses,
             }
-        ).transform(clean_count)
-        ds.people_vaccinated = ds.people_vaccinated + doses_unique
+        )
         return ds
 
     def _parse_unique_doses(self, driver):
@@ -57,8 +61,8 @@ class Brazil:
         time.sleep(5)
         path = get_latest_file(self._download_path, "xlsx")
         df = pd.read_excel(path)
-        doses_unique = df.loc[df.Fabricante == "JANSSEN", "Doses Aplicadas"].sum().item()
-        return doses_unique
+        unique_doses = df.loc[df.Fabricante == "JANSSEN", "Doses Aplicadas"].sum().item()
+        return unique_doses
 
     def pipe_date(self, ds: pd.Series) -> pd.Series:
         date = localdate("Brazil/East")
@@ -88,6 +92,7 @@ class Brazil:
             total_vaccinations=data["total_vaccinations"],
             people_vaccinated=data["people_vaccinated"],
             people_fully_vaccinated=data["people_fully_vaccinated"],
+            total_boosters=data["total_boosters"],
             date=data["date"],
             source_url=data["source_url"],
             vaccine=data["vaccine"],
