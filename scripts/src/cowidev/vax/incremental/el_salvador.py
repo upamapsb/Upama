9653e7348b08dde7a45a20d4bd84cf1c9442d333
lucas rg <lucasrg@kth.se>
2021-10-26 18:03:35 +0100

    fix(vax,el-salvador): fix inforgram element IDs

--- scripts/src/cowidev/vax/incremental/el_salvador.py
+++ scripts/src/cowidev/vax/incremental/el_salvador.py
@@ -21,7 +21,10 @@ class ElSalvador:
             {
                 "date": self.parse_infogram_date(infogram_data),
                 "source_url": self.source_url,
-                **self.parse_infogram_vaccinations(infogram_data),
+                "total_vaccinations": self._parse_total_vaccinations(infogram_data),
+                "people_vaccinated": self._parse_people_vaccinated(infogram_data),
+                "people_fully_vaccinated": self._parse_people_fully_vaccinated(infogram_data),
+                "total_boosters": self._parse_boosters(infogram_data),
             }
         )
 
@@ -43,26 +46,28 @@ class ElSalvador:
             return "".join(x["text"] for x in infogram_data[field_id]["props"]["content"]["blocks"])
         return infogram_data[field_id]["props"]["content"]["blocks"][0]["text"]
 
-    def parse_infogram_vaccinations(self, infogram_data: dict) -> int:
-        total_vaccinations = clean_count(
-            self._get_infogram_value(infogram_data, "5088d5fc-24f7-46db-bf7e-3234db46a262")
+    def _parse_total_vaccinations(self, infogram_data: dict) -> int:
+        return clean_count(self._get_infogram_value(infogram_data, "33f6ba96-fffd-4a95-a7ac-b65aa9f808c3"))
+
+    def _parse_people_vaccinated(self, infogram_data: dict) -> int:
+        ppl_vaxed = clean_count(self._get_infogram_value(infogram_data, "4275cc3f-7ae8-4af3-9c5a-ef94203d47d7"))
+        ppl_vaxed_for = clean_count(self._get_infogram_value(infogram_data, "8a007cb6-7384-4af1-9f92-c41699d77aab"))
+        return ppl_vaxed + ppl_vaxed_for
+
+    def _parse_people_fully_vaccinated(self, infogram_data: dict) -> int:
+        ppl_fully_vaxed = clean_count(self._get_infogram_value(infogram_data, "7b45d34f-b8d0-47d7-8c3a-35c89a4d4cdf"))
+        ppl_fully_vaxed_for = clean_count(
+            self._get_infogram_value(infogram_data, "9eee1f41-c398-4a15-81aa-2588250e53cb")
         )
-        people_vaccinated = clean_count(
-            self._get_infogram_value(infogram_data, "90b218fc-f246-4a2e-bc33-fa0af726fb67")
-        )
-        people_fully_vaccinated = clean_count(
-            self._get_infogram_value(infogram_data, "efc94320-fe88-4d58-abb6-4d703c5983dc")
-        )
-        total_boosters = clean_count(self._get_infogram_value(infogram_data, "12ece579-eb52-4622-b412-4c152c3fa457"))
-        return {
-            "total_vaccinations": total_vaccinations,
-            "people_vaccinated": people_vaccinated,
-            "people_fully_vaccinated": people_fully_vaccinated,
-            "total_boosters": total_boosters,
-        }
+        return ppl_fully_vaxed + ppl_fully_vaxed_for
+
+    def _parse_boosters(self, infogram_data: dict) -> int:
+        boosters = clean_count(self._get_infogram_value(infogram_data, "2fbd1738-f9c3-49ad-8855-c933c83abc18"))
+        boosters_for = clean_count(self._get_infogram_value(infogram_data, "20d53fe7-91f8-4778-a13f-c938f18dd8fe"))
+        return boosters + boosters_for
 
     def parse_infogram_date(self, infogram_data: dict) -> str:
-        x = self._get_infogram_value(infogram_data, "3f6fa939-ad51-436f-a5dd-d6952609d242", join_text=True)
+        x = self._get_infogram_value(infogram_data, "d58d673d-f6f7-44d2-8825-8f83ea806a69", join_text=True)
         dt = extract_clean_date(x, "RESUMEN DE VACUNACIÓN\s?(\d+-[A-Z]+-2\d)\s?", "%d-%b-%y", lang="es")
         return dt
 
