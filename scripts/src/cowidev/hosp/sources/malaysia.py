8c8b5e9d506ece1fcea104c4855af44c6967ae8e
Edouard Mathieu <edmat@pm.me>
2021-11-03 12:01:00 +0100

    feat(hosp): add hosp & ICU data for Malaysia

---
+++ scripts/src/cowidev/hosp/sources/malaysia.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def main():
+
+    print("Downloading Malaysia data…")
+
+    url = "https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/hospital.csv"
+    hosp = pd.read_csv(url, usecols=["date", "hosp_covid"]).groupby("date", as_index=False).sum()
+
+    url = "https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/icu.csv"
+    icu = pd.read_csv(url, usecols=["date", "icu_covid"]).groupby("date", as_index=False).sum()
+
+    df = (
+        pd.merge(hosp, icu, on="date", how="outer", validate="one_to_one")
+        .melt(id_vars="date", var_name="indicator")
+        .assign(entity="Malaysia", iso_code="MYS", population=32776195)
+    )
+
+    df.loc[:, "indicator"] = df.indicator.replace(
+        {
+            "hosp_covid": "Daily hospital occupancy",
+            "icu_covid": "Daily ICU occupancy",
+        }
+    )
+
+    return df
