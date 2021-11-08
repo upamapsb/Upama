72bbced09d1febe62927c28f7e376f0c5953b173
Edouard Mathieu <edmat@pm.me>
2021-11-08 11:53:23 +0100

    fix(vax,malaysia): refactor to simplify calculations

--- scripts/src/cowidev/vax/batch/malaysia.py
+++ scripts/src/cowidev/vax/batch/malaysia.py
@@ -1,6 +1,6 @@
-import re
-
 import pandas as pd
+
+from cowidev.vax.utils.utils import build_vaccine_timeline
 
 
 class Malaysia:
@@ -8,84 +8,70 @@ class Malaysia:
         self.location = "Malaysia"
         self.source_url = "https://github.com/CITF-Malaysia/citf-public/raw/main/vaccination/vax_malaysia.csv"
         self.source_url_ref = "https://github.com/CITF-Malaysia/citf-public"
-        self.columns_rename = {
-            "date": "date",
-            "cumul_full": "people_fully_vaccinated",
-            "cumul": "total_vaccinations",
-            "cumul_booster": "total_boosters",
-        }
-        # From https://github.com/CITF-Malaysia/citf-public/tree/main/vaccination
-        self._columns_default = [
-            "date",
-            "daily_partial",
-            "daily_full",
-            "daily",
-            "cumul_partial",
-            "cumul_full",
-            "cumul",
-            "pending",
-            "cumul_full_child",
-            "cumul_partial_child",
-            "daily_full_child",
-            "daily_partial_child",
-            "cumul_booster",
-            "daily_booster",
-        ]
-        self._vax_2d = {
+        self._vax_2d = [
             "pfizer",
             "astra",
             "sinovac",
-        }
-        self._vax_1d = {
+            "sinopharm",
+        ]
+        self._vax_1d = [
             "cansino",
-        }
+        ]
 
     def read(self) -> pd.DataFrame:
-        return pd.read_csv(
-            self.source_url,
-        )
+        return pd.read_csv(self.source_url)
 
-    def pipe_rename_columns(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df.rename(columns=self.columns_rename)
-
-    def _check_double_vaccines(self, df: pd.DataFrame):
-        reg = r"([a-z]+)(?:1|2)"
-        columns_2dose = df.filter(regex=reg).columns.tolist()
-        vaccines_2dose = {re.search(reg, col).group(1) for col in columns_2dose}
-        vaccines_2dose_wrong = vaccines_2dose.difference(self._vax_2d)
-        if vaccines_2dose_wrong:
-            raise ValueError(f"New double-vaccine(s): {vaccines_2dose_wrong}")
-        return columns_2dose
-
-    @property
-    def columns_1dose(self):
-        return list(self._vax_1d)
-
-    def pipe_check_vaccines(self, df: pd.DataFrame) -> pd.DataFrame:
-        # Get double-dose vaccines
-        columns_2dose = self._check_double_vaccines(df)
-        columns_new = df.columns.difference(self._columns_default + self.columns_1dose + columns_2dose).tolist()
-        if columns_new:
-            raise ValueError(
-                f"New columns {columns_new}! If single-shot data, need to correct variable `people_vaccinated` in "
-                "method `pipe_correct_single_shot()`"
+    def pipe_check_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        expected_cols = 28
+        if df.shape[1] > expected_cols:
+            print(df.columns)
+            raise Exception(
+                f"More columns ({df.shape[1]}) than expected ({expected_cols}) are present. Check for new vaccines?"
             )
         return df
 
-    def pipe_correct_single_shot(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df.assign(people_vaccinated=(df.cumul_partial + df[self._vax_1d].sum(axis=1)).astype(int))
+    def pipe_filter_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        all_vaccines = self._vax_2d + self._vax_1d + ["date"]
+        reg = "|".join(all_vaccines)
+        columns_kept = df.filter(regex=reg).columns.tolist()
+        df = df[columns_kept].rename(columns={"cansino": "cansino1"})
+        return df
 
-    def pipe_vaccine(self, df: pd.DataFrame) -> str:
-        def _enrich_vaccine(date):
-            if date >= "2021-08-27":
-                return "CanSino, Oxford/AstraZeneca, Pfizer/BioNTech, Sinovac"
-            if date >= "2021-05-05":
-                return "Oxford/AstraZeneca, Pfizer/BioNTech, Sinovac"
-            if date >= "2021-03-17":
-                return "Pfizer/BioNTech, Sinovac"
-            return "Pfizer/BioNTech"
+    def pipe_calculate_metrics(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.melt(id_vars="date", var_name="vaccine", value_name="doses")
+        df["dose_number"] = df.vaccine.str.extract(r"(\d+)$").astype(int)
+        df["vaccine"] = df.vaccine.str.replace(r"(\d+)$", "")
 
-        return df.assign(vaccine=df.date.astype(str).apply(_enrich_vaccine))
+        df = df.pivot(index=["date", "vaccine"], columns="dose_number", values="doses").reset_index().fillna(0)
+
+        # total_vaccinations
+        df["total_vaccinations"] = df[1] + df[2] + df[3]
+
+        # people_vaccinated
+        df["people_vaccinated"] = df[1]
+
+        # people_fully_vaccinated
+        df.loc[df.vaccine.isin(self._vax_2d), "people_fully_vaccinated"] = df[2]
+        df.loc[df.vaccine.isin(self._vax_1d), "people_fully_vaccinated"] = df[1]
+
+        # total_boosters
+        df.loc[df.vaccine.isin(self._vax_2d), "total_boosters"] = df[3]
+        df.loc[df.vaccine.isin(self._vax_1d), "total_boosters"] = df[2] + df[3]
+
+        df = (
+            df[["date", "total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "total_boosters"]]
+            .groupby("date", as_index=False)
+            .sum()
+            .sort_values("date")
+        )
+
+        df[["total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "total_boosters"]] = (
+            df[["total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "total_boosters"]]
+            .cumsum()
+            .astype(int)
+        )
+
+        return df
 
     def pipe_metadata(self, df: pd.DataFrame) -> pd.DataFrame:
         return df.assign(
@@ -109,10 +95,19 @@ class Malaysia:
 
     def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
         return (
-            df.pipe(self.pipe_check_vaccines)
-            .pipe(self.pipe_rename_columns)
-            .pipe(self.pipe_correct_single_shot)
-            .pipe(self.pipe_vaccine)
+            df.pipe(self.pipe_filter_columns)
+            .pipe(self.pipe_check_columns)
+            .pipe(self.pipe_calculate_metrics)
+            .pipe(
+                build_vaccine_timeline,
+                {
+                    "Pfizer/BioNTech": "2021-02-24",
+                    "Sinovac": "2021-03-03",
+                    "Oxford/AstraZeneca": "2021-05-03",
+                    "CanSino": "2021-05-09",
+                    "Sinopharm/Beijing": "2021-09-18",
+                },
+            )
             .pipe(self.pipe_metadata)
             .pipe(self.pipe_columns_out)
         )
