ea3ecb233819acdc787b9d5084c987741ee01c60
Edouard Mathieu <edmat@pm.me>
2021-11-16 11:13:32 +0100

    feat(vax,argentina): switch source + add booster data
    Public-facing dashboard is more up to date, and contains data on booster doses

--- scripts/src/cowidev/vax/incremental/argentina.py
+++ scripts/src/cowidev/vax/incremental/argentina.py
@@ -1,3 +1,5 @@
+import requests
+
 import pandas as pd
 
 from cowidev.vax.utils.incremental import enrich_data, increment
@@ -5,100 +7,74 @@ from cowidev.utils.clean.dates import localdate
 
 
 class Argentina:
-    location: str = "Argentina"
-    source_url_ref: str = (
-        "http://datos.salud.gob.ar/dataset/vacunas-contra-covid-19-dosis-aplicadas-en-la-republica-argentina"
-    )
-    source_url: str = "https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19VacunasAgrupadas.csv.zip"
-    vaccine_mapping: dict = {
-        "AstraZeneca ChAdOx1 S recombinante": "Oxford/AstraZeneca",
-        "Cansino Ad5 nCoV": "CanSino",
-        "COVISHIELD ChAdOx1nCoV COVID 19": "Oxford/AstraZeneca",
-        "Moderna ARNm": "Moderna",
-        "Pfizer BioNTech Comirnaty": "Pfizer/BioNTech",
-        "Sinopharm Vacuna SARSCOV 2 inactivada": "Sinopharm/Beijing",
-        "Sputnik V COVID19 Instituto Gamaleya": "Sputnik V",
-        # "Vacuna COVID Estudios Clínicos"
-    }
-    one_dose_vaccine: list = ["Cansino Ad5 nCoV"]
+    location = "Argentina"
+    source_url_ref = "https://www.argentina.gob.ar/coronavirus/vacuna/aplicadas"
+    source_url = "https://coronavirus.msal.gov.ar/vacunas/d/8wdHBOsMk/seguimiento-vacunacion-covid/api/datasources/proxy/1/query"
 
     def read(self) -> pd.DataFrame:
-        return pd.read_csv(
-            self.source_url,
-            # usecols=[
-            #     "primera_dosis_cantidad",
-            #     "segunda_dosis_cantidad",
-            #     "vacuna_nombre",
-            # ],
-        )
 
-    def pipe_vaccine_checks(self, df: pd.DataFrame) -> pd.DataFrame:
-        vaccines_known = {*set(self.vaccine_mapping)}  # , "Vacuna COVID Estudios Clínicos"}
-        vaccines_unknown = set(df.vacuna_nombre).difference(vaccines_known)
-        if vaccines_unknown:
-            raise ValueError(f"Unknown vaccine name(s): {vaccines_unknown}")
-        return df
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+            "Accept": "application/json, text/plain, */*",
+            "Referer": "https://coronavirus.msal.gov.ar/vacunas/d/8wdHBOsMk/seguimiento-vacunacion-covid/d/8wdHBOsMk/seguimiento-vacunacion-covid?orgId=1&refresh=15m%3F",
+            "content-type": "application/json",
+            "x-grafana-org-id": "1",
+            "Origin": "https://coronavirus.msal.gov.ar",
+            "Connection": "keep-alive",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+        }
+        query = '{"app":"dashboard","requestId":"Q101","timezone":"","panelId":4,"dashboardId":3,"range":{"raw":{"from":"2020-12-29T03:00:00.000Z","to":"now"}},"timeInfo":"","interval":"6h","intervalMs":21600000,"targets":[{"data":null,"target":"distribucion_aplicacion_utilidad_provincia_tabla_publico","refId":"A","hide":false,"type":"table"}],"maxDataPoints":9999,"scopedVars":{"__from":{"text":"1609210800000","value":"1609210800000"},"__dashboard":{"value":{"name":"Seguimiento vacunaci\xf3n Covid","uid":"8wdHBOsMk"}},"__org":{"value":{"name":"minsal","id":0}},"__interval":{"text":"6h","value":"6h"},"__interval_ms":{"text":"21600000","value":21600000}},"startTime":1637056461969,"rangeRaw":{"from":"2020-12-29T03:00:00.000Z","to":"now"},"adhocFilters":[]}'
 
-    def pipe_metrics(self, df: pd.DataFrame) -> pd.Series:
-        mask = df.vacuna_nombre.isin(self.one_dose_vaccine)
-        df_1d = df.loc[mask]
-        df_nd = df.loc[~mask]
-        single_shots = df_1d["primera_dosis_cantidad"].sum()
-        first_shots = df_nd["primera_dosis_cantidad"].sum()
-        second_shots = df_nd["segunda_dosis_cantidad"].sum()
+        json_data = requests.post(self.source_url, headers=headers, data=query).json()
+        for row in json_data[0]["rows"]:
+            if row[0] == "Totales":
+                data = row
+                break
 
-        if (df_1d["segunda_dosis_cantidad"] > 2).all():
-            raise ValueError(
-                f"`segunda_dosis_cantidad` field for one dose vaccines ({self.one_dose_vaccine}) must be at most 2 for"
-                " all regions!"
-            )
-        vaccines = set(df.vacuna_nombre.replace(self.vaccine_mapping))
-        # vaccines.remove("Vacuna COVID Estudios Clínicos")
-        print(vaccines)
-        vaccines = ", ".join(sorted(vaccines))
-        return pd.Series(
+        data = pd.Series(
             {
-                "people_vaccinated": first_shots + single_shots,
-                "people_fully_vaccinated": second_shots + single_shots,
-                "total_vaccinations": df["primera_dosis_cantidad"].sum() + df["segunda_dosis_cantidad"].sum(),
-                "vaccine": vaccines,
+                "people_vaccinated": data[2],
+                "people_fully_vaccinated": data[3],
+                "total_vaccinations": data[7],
+                "total_boosters": data[5] + data[6],
             }
         )
+        assert data.total_vaccinations >= data.people_vaccinated >= data.people_fully_vaccinated
+        assert data.total_vaccinations >= data.total_boosters
+
+        return data
 
     def pipe_date(self, ds: pd.Series) -> pd.Series:
-        date = localdate("America/Argentina/Buenos_Aires", 8)
-        return enrich_data(ds, "date", date)
+        return enrich_data(ds, "date", localdate("America/Argentina/Buenos_Aires"))
 
     def pipe_location(self, ds: pd.Series) -> pd.Series:
         return enrich_data(ds, "location", self.location)
 
     def pipe_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "source_url", self.source_url_ref)
+
+    def pipe_vaccines(self, ds: pd.Series) -> pd.Series:
         return enrich_data(
-            ds,
-            "source_url",
-            self.source_url_ref,
+            ds, "vaccine", "CanSino, Moderna, Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing, Sputnik V"
         )
 
     def pipeline(self, ds: pd.Series) -> pd.Series:
-        return (
-            ds.pipe(self.pipe_vaccine_checks)
-            .pipe(self.pipe_metrics)
-            .pipe(self.pipe_date)
-            .pipe(self.pipe_location)
-            .pipe(self.pipe_source)
-        )
+        return ds.pipe(self.pipe_date).pipe(self.pipe_location).pipe(self.pipe_source).pipe(self.pipe_vaccines)
 
     def export(self, paths):
         data = self.read().pipe(self.pipeline)
         increment(
             paths=paths,
-            location=str(data["location"]),
-            total_vaccinations=int(data["total_vaccinations"]),
-            people_vaccinated=int(data["people_vaccinated"]),
-            people_fully_vaccinated=int(data["people_fully_vaccinated"]),
-            date=str(data["date"]),
-            source_url=str(data["source_url"]),
-            vaccine=str(data["vaccine"]),
+            location=data["location"],
+            total_vaccinations=data["total_vaccinations"],
+            people_vaccinated=data["people_vaccinated"],
+            people_fully_vaccinated=data["people_fully_vaccinated"],
+            total_boosters=data["total_boosters"],
+            date=data["date"],
+            source_url=data["source_url"],
+            vaccine=data["vaccine"],
         )
 
 
