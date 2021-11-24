821305634a6ee30bcb8aa6b6fd79a20528f5c5b4
Edouard Mathieu <edmat@pm.me>
2021-11-24 11:20:25 -0500

    enhance(vax,indonesia): use easier source

--- scripts/src/cowidev/vax/incremental/indonesia.py
+++ scripts/src/cowidev/vax/incremental/indonesia.py
@@ -1,57 +1,29 @@
-from bs4 import BeautifulSoup
+import requests
 
 import pandas as pd
-import requests
-import json
-import re
 
 from cowidev.utils.clean.dates import localdate
-from cowidev.utils.web.scraping import get_soup
 from cowidev.vax.utils.incremental import enrich_data, increment
 
 
 class Indonesia:
-    location: str = "Indonesia"
-    source_url_ref: str = "https://vaksin.kemkes.go.id/#/vaccines"
-    source_url_dose_1: str = (
-        "https://public.tableau.com/views/DashboardVaksinKemkes/TotalVaksinasiDosis1?:embed=yes&:showVizHome=no"
-    )
-    source_url_dose_2: str = (
-        "https://public.tableau.com/views/DashboardVaksinKemkes/TotalVaksinasiDosis2?:embed=yes&:showVizHome=no"
-    )
+    location = "Indonesia"
+    source_url_ref = "https://data.covid19.go.id/public/index.html"
+    source_url = "https://data.covid19.go.id/public/api/pemeriksaan-vaksinasi.json"
 
     def read(self) -> pd.Series:
-        dose1_soup = get_soup(self.source_url_dose_1)
-        dose2_soup = get_soup(self.source_url_dose_2)
-        return self._parse_data(dose1_soup, dose2_soup)
-
-    def _parse_data(self, dose1_soup: BeautifulSoup, dose2_soup: BeautifulSoup) -> pd.Series:
-        dose1 = self._parse_tableau(dose1_soup)
-        dose2 = self._parse_tableau(dose2_soup)
-        data = pd.Series(
+        data = requests.get(self.source_url).json()
+        data = data["vaksinasi"]["total"]
+        return pd.Series(
             {
-                "people_vaccinated": dose1,
-                "people_fully_vaccinated": dose2,
-                "total_vaccinations": dose1 + dose2,
+                "people_vaccinated": data["jumlah_vaksinasi_1"],
+                "people_fully_vaccinated": data["jumlah_vaksinasi_2"],
+                "total_vaccinations": data["jumlah_vaksinasi_1"] + data["jumlah_vaksinasi_2"],
             }
         )
-        return data
-
-    def _parse_tableau(self, soup: BeautifulSoup) -> int:
-        tableauData = json.loads(soup.find("textarea", {"id": "tsConfigContainer"}).text)
-        dataUrl = (
-            f'https://public.tableau.com{tableauData["vizql_root"]}/bootstrapSession/sessions/'
-            f'{tableauData["sessionid"]}'
-        )
-        r = requests.post(dataUrl, data={"sheet_id": tableauData["sheetId"]})
-        dataReg = re.search(r"\d+;({.*})\d+;({.*})", r.text, re.MULTILINE)
-        data = json.loads(dataReg.group(2))
-        return data["secondaryInfo"]["presModelMap"]["dataDictionary"]["presModelHolder"][
-            "genDataDictionaryPresModel"
-        ]["dataSegments"]["0"]["dataColumns"][0]["dataValues"][0]
 
     def pipe_date(self, ds: pd.Series) -> pd.Series:
-        return enrich_data(ds, "date", localdate("Asia/Jakarta", force_today=True))
+        return enrich_data(ds, "date", localdate("Asia/Jakarta"))
 
     def pipe_location(self, ds: pd.Series) -> pd.Series:
         return enrich_data(ds, "location", self.location)
