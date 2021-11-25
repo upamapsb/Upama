86cf040739682a466e4b4cb0f8fbcd2d11068d8f
lucas rg <lucasrg@kth.se>
2021-11-25 12:03:01 +0100

    fix(vax,bangladesh): increased timeout

--- scripts/src/cowidev/vax/incremental/bangladesh.py
+++ scripts/src/cowidev/vax/incremental/bangladesh.py
@@ -8,58 +8,58 @@ from cowidev.utils.web.scraping import get_soup
 from cowidev.vax.utils.incremental import enrich_data, increment
 
 
-def read(source: str) -> pd.Series:
+class Bangladesh:
+    location: str = "Bangladesh"
+    source_url: str = "http://103.247.238.92/webportal/pages/covid19-vaccination-update.php"
 
-    soup = get_soup(source)
+    def read(self) -> pd.Series:
 
-    people_vaccinated = clean_count(re.search(r"^[\d,]+", soup.find_all(class_="info-box-number")[2].text).group(0))
-    people_fully_vaccinated = clean_count(
-        re.search(r"^[\d,]+", soup.find_all(class_="info-box-number")[3].text).group(0)
-    )
-    total_vaccinations = people_vaccinated + people_fully_vaccinated
+        soup = get_soup(self.source_url, timeout=30)
 
-    date = localdate("Asia/Dhaka")
+        people_vaccinated = clean_count(
+            re.search(r"^[\d,]+", soup.find_all(class_="info-box-number")[2].text).group(0)
+        )
+        people_fully_vaccinated = clean_count(
+            re.search(r"^[\d,]+", soup.find_all(class_="info-box-number")[3].text).group(0)
+        )
+        total_vaccinations = people_vaccinated + people_fully_vaccinated
 
-    return pd.Series(
-        data={
-            "total_vaccinations": total_vaccinations,
-            "people_vaccinated": people_vaccinated,
-            "people_fully_vaccinated": people_fully_vaccinated,
-            "date": date,
-        }
-    )
+        date = localdate("Asia/Dhaka")
 
+        return pd.Series(
+            data={
+                "total_vaccinations": total_vaccinations,
+                "people_vaccinated": people_vaccinated,
+                "people_fully_vaccinated": people_fully_vaccinated,
+                "date": date,
+            }
+        )
 
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Bangladesh")
+    def pipe_location(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "location", "Bangladesh")
 
+    def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "vaccine", "Moderna, Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing")
 
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Moderna, Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing")
+    def pipe_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "source_url", self.source_url)
 
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return ds.pipe(self.pipe_location).pipe(self.pipe_vaccine).pipe(self.pipe_source)
 
-def enrich_source(ds: pd.Series, source: str) -> pd.Series:
-    return enrich_data(ds, "source_url", source)
-
-
-def pipeline(ds: pd.Series, source: str) -> pd.Series:
-    return ds.pipe(enrich_location).pipe(enrich_vaccine).pipe(enrich_source, source)
+    def export(self, paths):
+        data = self.read().pipe(self.pipeline)
+        increment(
+            paths=paths,
+            location=data["location"],
+            total_vaccinations=data["total_vaccinations"],
+            people_vaccinated=data["people_vaccinated"],
+            people_fully_vaccinated=data["people_fully_vaccinated"],
+            date=data["date"],
+            source_url=data["source_url"],
+            vaccine=data["vaccine"],
+        )
 
 
 def main(paths):
-    source = "http://103.247.238.92/webportal/pages/covid19-vaccination-update.php"
-    data = read(source).pipe(pipeline, source)
-    increment(
-        paths=paths,
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"],
-    )
-
-
-if __name__ == "__main__":
-    main()
+    Bangladesh().export(paths)
