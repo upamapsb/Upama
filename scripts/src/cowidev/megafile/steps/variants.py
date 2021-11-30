b754a43fac63d52ee33740ec7f6bc835331543b7
Edouard Mathieu <edmat@pm.me>
2021-11-30 10:58:53 -0500

    fix(variants): calculate share_cases_sequenced per 100

--- scripts/src/cowidev/megafile/steps/variants.py
+++ scripts/src/cowidev/megafile/steps/variants.py
@@ -5,6 +5,6 @@ def get_variants(cases_file: str, variants_file: str) -> pd.DataFrame:
     variants = pd.read_csv(variants_file, usecols=["location", "date", "num_sequences_total"]).drop_duplicates()
     cases = pd.read_csv(cases_file, usecols=["location", "date", "biweekly_cases"]).dropna()
     df = pd.merge(variants, cases, how="inner", on=["location", "date"], validate="one_to_one")
-    df["share_cases_sequenced"] = 1000 * df.num_sequences_total / df.biweekly_cases
+    df["share_cases_sequenced"] = df.num_sequences_total.mul(100).div(df.biweekly_cases).round(1)
     df = df.drop(columns=["num_sequences_total", "biweekly_cases"])
     return df
