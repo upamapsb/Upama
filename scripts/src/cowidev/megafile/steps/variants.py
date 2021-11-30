6d113b6410e5411d6b621670b1047e7e48867c51
Edouard Mathieu <edmat@pm.me>
2021-11-30 11:04:39 -0500

    fix(variants): remove rows with >100% cases sequenced

--- scripts/src/cowidev/megafile/steps/variants.py
+++ scripts/src/cowidev/megafile/steps/variants.py
@@ -6,5 +6,6 @@ def get_variants(cases_file: str, variants_file: str) -> pd.DataFrame:
     cases = pd.read_csv(cases_file, usecols=["location", "date", "biweekly_cases"]).dropna()
     df = pd.merge(variants, cases, how="inner", on=["location", "date"], validate="one_to_one")
     df["share_cases_sequenced"] = df.num_sequences_total.mul(100).div(df.biweekly_cases).round(1)
+    df = df[df.share_cases_sequenced <= 100]
     df = df.drop(columns=["num_sequences_total", "biweekly_cases"])
     return df
