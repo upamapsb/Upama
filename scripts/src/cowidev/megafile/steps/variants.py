93bf2e34cac58e54bc0cdf80362a36886f63f9c5
Edouard Mathieu <edmat@pm.me>
2021-11-30 12:10:13 -0500

    enhance(variants): add 2-week cutoff to share sequenced

--- scripts/src/cowidev/megafile/steps/variants.py
+++ scripts/src/cowidev/megafile/steps/variants.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 
 
@@ -7,5 +9,6 @@ def get_variants(cases_file: str, variants_file: str) -> pd.DataFrame:
     df = pd.merge(variants, cases, how="inner", on=["location", "date"], validate="one_to_one")
     df["share_cases_sequenced"] = df.num_sequences_total.mul(100).div(df.biweekly_cases).round(1)
     df = df[df.share_cases_sequenced <= 100]
+    df = df[df.date <= str(datetime.date.today() - datetime.timedelta(days=15))]
     df = df.drop(columns=["num_sequences_total", "biweekly_cases"])
     return df
