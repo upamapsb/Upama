a647c9c22a95e0d3b5504cb0b80fae7b8deeb7c9
Edouard Mathieu <edmat@pm.me>
2021-12-02 08:52:32 -0500

    feat(hosp): add South Africa

---
+++ scripts/src/cowidev/hosp/sources/south_africa.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def main() -> pd.DataFrame:
+
+    print("Downloading South Africa data…")
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRGCkIwakQ5rpfXky9FZhDwr3qUgerfhBLSzn9OsA79yQ_2G_y-_Ns9JjRJZWXD5kxJ3qicoL7bHGjE/pub?gid=1044172863&single=true&output=csv"
+    df = pd.read_csv(url, usecols=["Week ending:", "National"])
+
+    df = df.assign(indicator="Weekly new hospital admissions").rename(
+        columns={
+            "Week ending:": "date",
+            "National": "value",
+        }
+    )
+    df["date"] = pd.to_datetime(df.date, dayfirst=True)
+
+    df.loc[:, "entity"] = "South Africa"
+    df.loc[:, "iso_code"] = "ZAF"
+    df.loc[:, "population"] = 60041996
+
+    return df
