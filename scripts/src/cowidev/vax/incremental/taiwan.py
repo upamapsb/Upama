0876018cfa778c3d7eb85d035b9a146a92263469
lucas rg <lucasrg@kth.se>
2021-12-03 13:56:56 +0100

    fix(vax,taiwan): change in source table format

--- scripts/src/cowidev/vax/incremental/taiwan.py
+++ scripts/src/cowidev/vax/incremental/taiwan.py
@@ -11,18 +11,19 @@ from cowidev.vax.utils.incremental import enrich_data, increment
 # New vaccine "Medigen" soon:
 # https://www.cdc.gov.tw/En/Bulletin/Detail/5NuaA-4jqd9nSh03MdwiWw?typeid=158
 # https://www.cdc.gov.tw/En/Bulletin/Detail/SEd8rAKMzywG_b92N6z8nA?typeid=158
-vaccines_mapping = {
-    "AstraZeneca": "Oxford/AstraZeneca",
-    "高端": "Medigen",
-    "Moderna": "Moderna",
-    "BioNTech": "Pfizer/BioNTech",
-}
 
 
 class Taiwan:
     def __init__(self):
         self.source_url = "https://www.cdc.gov.tw"
         self.location = "Taiwan"
+        self.columns_accepted = {"廠牌", "劑次", "12/2接種劑數", "累計至 12/2接種劑數"}
+        self.vaccines_mapping = {
+            "AstraZeneca": "Oxford/AstraZeneca",
+            "高端": "Medigen",
+            "Moderna": "Moderna",
+            "BioNTech": "Pfizer/BioNTech",
+        }
 
     @property
     def source_data_url(self):
@@ -31,20 +32,8 @@ class Taiwan:
     def read(self) -> pd.Series:
         soup = get_soup(self.source_data_url)
         url_pdf = self._parse_pdf_link(soup)
-        dfs = self._parse_tables(url_pdf)
-        data = self.parse_data(dfs, soup)
-        return data
-
-    def parse_data(self, dfs: list, soup):
-        stats = self._parse_stats(dfs[0])
-        data = pd.Series(
-            {
-                "total_vaccinations": stats["total_vaccinations"],
-                "people_vaccinated": stats["people_vaccinated"],
-                "date": self._parse_date(soup),
-                "vaccine": self._parse_vaccines(dfs[0]),
-            }
-        )
+        df = self._parse_table(url_pdf)
+        data = self.parse_data(df, soup)
         return data
 
     def _parse_pdf_link(self, soup) -> str:
@@ -59,41 +48,75 @@ class Taiwan:
                 break
         return f"{self.source_url}{a['href']}"
 
-    def _parse_tables(self, url_pdf: str) -> int:
-        kwargs = {"pandas_options": {"dtype": str, "header": None}}
+    def _parse_table(self, url_pdf: str):
+        dfs = self._parse_tables_all(url_pdf)
+        df = dfs[0]
+        # Sanity check
+        cols_wrong = df.columns.difference(self.columns_accepted)
+        if cols_wrong.any():
+            raise ValueError(f"There are some unknown columns: {cols_wrong}")
+        # Fix index
+        index_ = df["廠牌"].iloc[:-1].shift().fillna(method="bfill")
+        df["廠牌"].iloc[:-1] = index_
+        df = df.set_index(["廠牌", "劑次"])
+        # Drop NaNs
+        df = df.dropna()
+        df.columns = ["daily", "total"]
+        return df
+
+    def _parse_tables_all(self, url_pdf: str) -> int:
+        kwargs = {"pandas_options": {"dtype": str, "header": 0}}
         dfs = tabula.read_pdf(url_pdf, pages="all", **kwargs)
         return dfs
 
+    def parse_data(self, df: pd.DataFrame, soup):
+        stats = self._parse_stats(df)
+        data = pd.Series(
+            {
+                "total_vaccinations": stats["total_vaccinations"],
+                "people_vaccinated": stats["people_vaccinated"],
+                "date": self._parse_date(soup),
+                "vaccine": self._parse_vaccines(df),
+            }
+        )
+        return data
+
     def _parse_stats(self, df: pd.DataFrame) -> int:
-        if (
-            (df.shape[1] != 3 and df.shape[1] != 4)
-            or df.iloc[0, 0] != "廠牌"
-            or df.iloc[0, 1] != "劑次"
-            or not (df.iloc[-1, 0] == "總計" or df.iloc[-2, 0] == "總計")
-        ):
+        # # Old
+        # if (
+        #     (df.shape[1] != 3 and df.shape[1] != 4)
+        #     or df.iloc[0, 0] != "廠牌"
+        #     or df.iloc[0, 1] != "劑次"
+        #     or df.iloc[-1, 0] != "總計"
+        #     or df.iloc[-2, 0] != "總計"
+        # ):
+        #     raise ValueError(f"Table 1: format has changed!")
+        # num1 = df[df[1] == "第 1劑"].tail(1).values[0][-1]
+        # num2 = df[df[1] == "第 2劑"].tail(1).values[0][-1]
+
+        # if df.shape[1] == 4:
+        #     num_dose1 = clean_count(num1)
+        #     num_dose2 = clean_count(num2)
+        # if df.shape[1] == 3:
+        #     num_dose1 = clean_count(num1.split(" ")[-1])
+        #     num_dose2 = clean_count(num2.split(" ")[-1])
+
+        if df.shape != (11, 2):
             raise ValueError(f"Table 1: format has changed!")
 
-        num1 = df[df[1] == "第 1劑"].tail(1).values[0][-1]
-        num2 = df[df[1] == "第 2劑"].tail(1).values[0][-1]
-
-        if df.shape[1] == 4:
-            num_dose1 = clean_count(num1)
-            num_dose2 = clean_count(num2)
-        if df.shape[1] == 3:
-            num_dose1 = clean_count(num1.split(" ")[-1])
-            num_dose2 = clean_count(num2.split(" ")[-1])
-
+        num_dose1 = clean_count(df.loc["總計", "第 1劑"]["total"])
+        num_dose2 = clean_count(df.loc["總計", "第 2劑"]["total"])
         return {
             "total_vaccinations": (num_dose1 + num_dose2),
             "people_vaccinated": num_dose1,
         }
 
     def _parse_vaccines(self, df: pd.DataFrame) -> str:
-        vaccines = set(df.iloc[1:-1, 0].dropna()) - set(["總計"])
-        vaccines_wrong = vaccines.difference(vaccines_mapping)
+        vaccines = set(df.index.levels[0]) - {"總計", "追加劑"}
+        vaccines_wrong = vaccines.difference(self.vaccines_mapping)
         if vaccines_wrong:
             raise ValueError(f"Invalid vaccines: {vaccines_wrong}")
-        return ", ".join(sorted(vaccines_mapping[vax] for vax in vaccines))
+        return ", ".join(sorted(self.vaccines_mapping[vax] for vax in vaccines))
 
     def _parse_date(self, soup) -> str:
         date_raw = soup.find(class_="download").text
