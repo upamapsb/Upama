388dc00b8bca74643e39a4c555b4f3333c2d3c9f
Edouard Mathieu <edmat@pm.me>
2021-12-09 11:25:40 +0100

    fix(vax,trinidad): correct for J&J

--- scripts/src/cowidev/vax/batch/trinidad_and_tobago.py
+++ scripts/src/cowidev/vax/batch/trinidad_and_tobago.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
+from cowidev.utils import paths
 from cowidev.utils.clean import clean_date_series
 from cowidev.utils.web import request_json
 from cowidev.vax.utils.files import load_query, load_data
-from cowidev.utils import paths
+from cowidev.vax.utils.utils import build_vaccine_timeline, make_monotonic
 
 
 def read(source: str) -> pd.DataFrame:
@@ -16,32 +17,44 @@ def parse_data(data: dict) -> int:
     records = [
         {
             "date": x["attributes"]["report_date_str"],
-            "people_vaccinated": x["attributes"]["total_vaccinated"],
+            "people_vaccinated_2dosevax": x["attributes"]["total_vaccinated"],
             "people_fully_vaccinated": x["attributes"]["total_second_dose"],
+            "j_and_j": x["attributes"]["fd_j_and_j"],
+            "d1_pfizer": x["attributes"]["fd_pfizer"],
+            "d1_sinopharm": x["attributes"]["fd_sinopharm"],
         }
         for x in data["features"]
     ]
     return pd.DataFrame.from_records(records)
 
 
-def process_nans(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(people_fully_vaccinated=df.people_fully_vaccinated.fillna(0))
-
-
-def add_totals(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(total_vaccinations=df.people_vaccinated + df.people_fully_vaccinated)
-
-
 def format_date(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(date=clean_date_series(df.date, "%d/%m/%Y"))
+    return df.assign(date=clean_date_series(df.date, "%d/%m/%Y")).sort_values("date")
+
+
+def enrich_vaccine_name(df: pd.DataFrame) -> pd.DataFrame:
+    vaccine_timeline = {
+        "Oxford/AstraZeneca": "2021-02-15",
+        "Johnson&Johnson": df.loc[df.j_and_j.notnull(), "date"].min(),
+        "Pfizer/BioNTech": df.loc[df.d1_pfizer.notnull(), "date"].min(),
+        "Sinopharm/Beijing": df.loc[df.d1_sinopharm.notnull(), "date"].min(),
+    }
+    return (
+        df.pipe(build_vaccine_timeline, vaccine_timeline)
+        .drop(columns=["d1_pfizer", "d1_sinopharm"])
+        .dropna(subset=["people_vaccinated_2dosevax"])
+    )
+
+
+def calculate_metrics(df: pd.DataFrame) -> pd.DataFrame:
+    df["people_fully_vaccinated"] = df.people_fully_vaccinated.fillna(0)
+    df["people_vaccinated"] = df.people_vaccinated_2dosevax.fillna(0) + df.j_and_j.fillna(0)
+    df["total_vaccinations"] = df.people_vaccinated + df.people_fully_vaccinated
+    return df.drop(columns=["people_vaccinated_2dosevax", "j_and_j"])
 
 
 def enrich_location(df: pd.DataFrame) -> pd.DataFrame:
     return df.assign(location="Trinidad and Tobago")
-
-
-def enrich_vaccine_name(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(vaccine="Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing")
 
 
 def enrich_source(df: pd.DataFrame, source: str) -> pd.DataFrame:
@@ -56,13 +69,13 @@ def merge_legacy(df: pd.DataFrame) -> pd.DataFrame:
 
 def pipeline(df: pd.DataFrame, source: str) -> pd.DataFrame:
     return (
-        df.pipe(process_nans)
-        .pipe(add_totals)
-        .pipe(enrich_location)
+        df.pipe(format_date)
         .pipe(enrich_vaccine_name)
+        .pipe(calculate_metrics)
+        .pipe(enrich_location)
         .pipe(enrich_source, source)
-        .pipe(format_date)
         .pipe(merge_legacy)
+        .pipe(make_monotonic)
     )
 
 
