ffe732e716dcc96d4582d5cf48f68483cac7aebf
lucas rg <lucasrg@kth.se>
2021-12-09 21:16:47 +0100

    feat(grapher): allow for multiple column pivot

--- scripts/src/cowidev/grapher/files/grapher.py
+++ scripts/src/cowidev/grapher/files/grapher.py
@@ -14,12 +14,33 @@ class Grapheriser:
     fillna_0: bool = True
     pivot_column: str = None
     pivot_values: str = None
+    suffixes: list = None
     function_input: Callable = lambda x: x
     function_output: Callable = lambda x: x
 
     @property
     def columns_metadata(self) -> list:
         return ["Country", "Year"]
+
+    @property
+    def metric2suffix(self) -> dict:
+        if len(self.pivot_values_list) == len(self.suffixes_list):
+            return dict(zip(self.pivot_values_list, self.suffixes_list))
+        else:
+            raise ValueError(f"`suffixes` and `pivot_values` should be lists of the same length")
+
+    @property
+    def pivot_values_list(self) -> list:
+        if not isinstance(self.pivot_values, list):
+            return [self.pivot_values]
+        return self.pivot_values
+
+    @property
+    def suffixes_list(self) -> list:
+        suffizes = self.suffixes
+        if not isinstance(self.suffixes, list):
+            suffizes = [self.suffixes]
+        return ["" if s is None else s for s in suffizes]
 
     def columns_data(self, df: pd.DataFrame) -> list:
         return [col for col in df.columns if col not in self.columns_metadata]
@@ -29,7 +50,7 @@ class Grapheriser:
             return df.pivot(
                 index=[self.location, self.date],
                 columns=self.pivot_column,
-                values=self.pivot_values,
+                values=self.pivot_values_list,
             ).reset_index()
         return df
 
@@ -43,6 +64,19 @@ class Grapheriser:
             .assign(date=(df[self.date] - self.date_ref).dt.days)
             .rename(columns={"date": "Year"})
         ).copy()
+        return df
+
+    def pipe_normalize_columns(self, df):
+        def _normalize_column(column):
+            if len(column) != 2:
+                raise ValueError("Column is expected to have length 2")
+            if column[1]:
+                column_new = f"{column[1]}{self.metric2suffix.get(column[0], '')}"
+            else:
+                column_new = column[0]
+            return column_new
+
+        df.columns = [_normalize_column(xx) for xx in df.columns]
         return df
 
     def pipe_order_columns(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -67,6 +101,7 @@ class Grapheriser:
             df.pipe(self.function_input)
             .pipe(self.pipe_pivot)
             .pipe(self.pipe_metadata_columns)
+            .pipe(self.pipe_normalize_columns)
             .pipe(self.pipe_order_columns)
             .pipe(self.pipe_fillna)
             .pipe(self.function_output)
