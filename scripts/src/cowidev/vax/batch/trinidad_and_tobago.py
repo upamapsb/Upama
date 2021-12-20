47d39591391117ac4d2d168a1aab3077d242f9a5
lucas rg <lucasrg@kth.se>
2021-12-20 14:08:40 +0100

    fix(vax,trinidad-tobago): fix data and add boosters

--- scripts/src/cowidev/vax/batch/trinidad_and_tobago.py
+++ scripts/src/cowidev/vax/batch/trinidad_and_tobago.py
@@ -7,86 +7,121 @@ from cowidev.vax.utils.files import load_query, load_data
 from cowidev.vax.utils.utils import build_vaccine_timeline, make_monotonic
 
 
-def read(source: str) -> pd.DataFrame:
-    params = load_query("trinidad-and-tobago-metrics", to_str=False)
-    data = request_json(source, params=params)
-    return parse_data(data)
-
-
-def parse_data(data: dict) -> int:
-    records = [
-        {
-            "date": x["attributes"]["report_date_str"],
-            "people_vaccinated_2dosevax": x["attributes"]["total_vaccinated"],
-            "people_fully_vaccinated": x["attributes"]["total_second_dose"],
-            "j_and_j": x["attributes"]["fd_j_and_j"],
-            "d1_pfizer": x["attributes"]["fd_pfizer"],
-            "d1_sinopharm": x["attributes"]["fd_sinopharm"],
-        }
-        for x in data["features"]
-    ]
-    return pd.DataFrame.from_records(records)
-
-
-def format_date(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(date=clean_date_series(df.date, "%d/%m/%Y")).sort_values("date")
-
-
-def enrich_vaccine_name(df: pd.DataFrame) -> pd.DataFrame:
-    vaccine_timeline = {
-        "Oxford/AstraZeneca": "2021-02-15",
-        "Johnson&Johnson": df.loc[df.j_and_j.notnull(), "date"].min(),
-        "Pfizer/BioNTech": df.loc[df.d1_pfizer.notnull(), "date"].min(),
-        "Sinopharm/Beijing": df.loc[df.d1_sinopharm.notnull(), "date"].min(),
-    }
-    return (
-        df.pipe(build_vaccine_timeline, vaccine_timeline)
-        .drop(columns=["d1_pfizer", "d1_sinopharm"])
-        .dropna(subset=["people_vaccinated_2dosevax"])
-    )
-
-
-def calculate_metrics(df: pd.DataFrame) -> pd.DataFrame:
-    df["people_fully_vaccinated"] = df.people_fully_vaccinated.fillna(0)
-    df["people_vaccinated"] = df.people_vaccinated_2dosevax.fillna(0) + df.j_and_j.fillna(0)
-    df["total_vaccinations"] = df.people_vaccinated + df.people_fully_vaccinated
-    return df.drop(columns=["people_vaccinated_2dosevax", "j_and_j"])
-
-
-def enrich_location(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(location="Trinidad and Tobago")
-
-
-def enrich_source(df: pd.DataFrame, source: str) -> pd.DataFrame:
-    return df.assign(source_url=source)
-
-
-def merge_legacy(df: pd.DataFrame) -> pd.DataFrame:
-    df_legacy = load_data("trinidad-and-tobago-legacy")
-    df_legacy = df_legacy[~df_legacy.date.isin(df.date)]
-    return pd.concat([df, df_legacy]).sort_values("date")
-
-
-def pipeline(df: pd.DataFrame, source: str) -> pd.DataFrame:
-    return (
-        df.pipe(format_date)
-        .pipe(enrich_vaccine_name)
-        .pipe(calculate_metrics)
-        .pipe(enrich_location)
-        .pipe(enrich_source, source)
-        .pipe(merge_legacy)
-        .pipe(make_monotonic)
-    )
-
-
-def main():
+class TrinidadTobago:
     source_ref = "https://experience.arcgis.com/experience/59226cacd2b441c7a939dca13f832112/"
     source = (
         "https://services3.arcgis.com/x3I4DqUw3b3MfTwQ/arcgis/rest/services/service_7a519502598f492a9094fd0ad503cf80/"
         "FeatureServer/0/query"
     )
-    destination = paths.out_vax("Trinidad and Tobago")
-    read(source).pipe(pipeline, source_ref).to_csv(destination, index=False)
+    location: str = "Trinidad and Tobago"
+
+    def read(self) -> pd.DataFrame:
+        params = load_query("trinidad-and-tobago-metrics", to_str=False)
+        data = request_json(self.source, params=params)
+        return self._parse_data(data)
+
+    def _parse_data(self, data: dict) -> int:
+        records = [
+            {
+                "date": x["attributes"]["report_date_str"],
+                "people_vaccinated_2dosevax": x["attributes"]["total_vaccinated"],
+                "people_fully_vaccinated_2dosevax": x["attributes"]["sd_total_second_dose"],
+                "people_fully_vaccinated": x["attributes"]["total_second_dose"],
+                "d1_jj": x["attributes"]["fd_j_and_j"],
+                "d1_pfizer": x["attributes"]["fd_pfizer"],
+                "d1_sinopharm": x["attributes"]["fd_sinopharm"],
+                "total_boosters": x["attributes"]["additional_primary_dose"],
+            }
+            for x in data["features"]
+        ]
+        return pd.DataFrame.from_records(records)
+
+    def pipe_date(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.assign(date=clean_date_series(df.date, "%d/%m/%Y")).sort_values("date")
+
+    def pipe_vaccine_name(self, df: pd.DataFrame) -> pd.DataFrame:
+        vaccine_timeline = {
+            "Oxford/AstraZeneca": "2021-02-15",
+            "Johnson&Johnson": df.loc[df.d1_jj.notnull(), "date"].min(),
+            "Pfizer/BioNTech": df.loc[df.d1_pfizer.notnull(), "date"].min(),
+            "Sinopharm/Beijing": df.loc[df.d1_sinopharm.notnull(), "date"].min(),
+        }
+        return (
+            df.pipe(build_vaccine_timeline, vaccine_timeline)
+            .drop(columns=["d1_pfizer", "d1_sinopharm"])
+            .dropna(subset=["people_vaccinated_2dosevax"])
+        )
+
+    def pipe_checks(self, df: pd.DataFrame) -> pd.DataFrame:
+        df_ = df.dropna(subset=["people_fully_vaccinated_2dosevax", "d1_jj", "people_fully_vaccinated"], how="any")
+        diff = (
+            df_["people_fully_vaccinated_2dosevax"].fillna(method="ffill")
+            + df_["d1_jj"].fillna(method="ffill")
+            - df_["people_fully_vaccinated"]
+        ).apply(abs) / df_["people_fully_vaccinated"]
+        msk = diff > 0.01
+        if msk.any():
+            raise ValueError(f"Fully vasccinated != single_dose + second_dose ({len(df_[msk])}):\n {df_[msk]}")
+        return df
+
+    def pipe_metrics(self, df: pd.DataFrame) -> pd.DataFrame:
+        df["people_fully_vaccinated"] = df.people_fully_vaccinated.fillna(method="ffill")
+        df["people_vaccinated"] = df.people_vaccinated_2dosevax.fillna(method="ffill") + df.d1_jj.fillna(
+            method="ffill"
+        )
+        df["total_vaccinations"] = (
+            df.people_vaccinated.fillna(method="ffill")
+            + df.people_fully_vaccinated_2dosevax.fillna(method="ffill")
+            + df.total_boosters.fillna(method="ffill")
+        )
+        df["total_boosters"] = df.total_boosters.fillna(method="ffill")
+        return df
+
+    def pipe_location(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.assign(location="Trinidad and Tobago")
+
+    def pipe_source(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.assign(source_url=self.source)
+
+    def pipe_legacy(self, df: pd.DataFrame) -> pd.DataFrame:
+        df_legacy = load_data("trinidad-and-tobago-legacy")
+        df_legacy = df_legacy[~df_legacy.date.isin(df.date)]
+        return pd.concat([df, df_legacy]).sort_values("date")
+
+    def pipe_out_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df[
+            [
+                "location",
+                "date",
+                "vaccine",
+                "source_url",
+                "total_vaccinations",
+                "people_vaccinated",
+                "people_fully_vaccinated",
+                "total_boosters",
+            ]
+        ]
+
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df.pipe(self.pipe_date)
+            .pipe(self.pipe_vaccine_name)
+            .pipe(self.pipe_checks)
+            .pipe(self.pipe_metrics)
+            .pipe(self.pipe_location)
+            .pipe(self.pipe_source)
+            .pipe(self.pipe_legacy)
+            .pipe(self.pipe_out_columns)
+            .pipe(make_monotonic)
+        )
+
+    def export(self):
+        destination = paths.out_vax(self.location)
+        self.read().pipe(self.pipeline).to_csv(destination, index=False)
+
+
+def main():
+    TrinidadTobago().export()
 
 
 if __name__ == "__main__":
