6b39dc8d1cd022174e6c0a2fb05469b99010afcb
lucas rg <lucasrg@kth.se>
2021-12-22 16:29:39 +0100

    feat(vax,bahrain): add boosters

--- scripts/src/cowidev/vax/incremental/bahrain.py
+++ scripts/src/cowidev/vax/incremental/bahrain.py
@@ -7,73 +7,81 @@ from cowidev.utils.web import get_soup
 from cowidev.vax.utils.incremental import enrich_data, increment
 
 
-def read(source: str) -> pd.Series:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:86.0) Gecko/20100101 Firefox/86.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache",
-    }
-    soup = get_soup(source)
-    return parse_data(soup)
+class Bahrain:
+    location: str = "Bahrain"
+    source_url: str = "https://healthalert.gov.bh/en/"
 
+    def read(self) -> pd.Series:
+        soup = get_soup(self.source_url)
+        return self._parse_data(soup)
 
-def parse_data(soup: BeautifulSoup) -> pd.Series:
+    def _parse_data(self, soup: BeautifulSoup) -> pd.Series:
+        people_vaccinated = int(soup.find_all(class_="count")[0]["data-count"])
+        people_fully_vaccinated = int(soup.find_all(class_="count")[1]["data-count"])
+        booster_shots = int(soup.find_all(class_="count")[2]["data-count"])
+        assert people_vaccinated >= people_fully_vaccinated
+        total_vaccinations = people_vaccinated + people_fully_vaccinated + booster_shots
 
-    people_vaccinated = int(soup.find_all(class_="count")[0]["data-count"])
-    people_fully_vaccinated = int(soup.find_all(class_="count")[1]["data-count"])
-    booster_shots = int(soup.find_all(class_="count")[2]["data-count"])
-    assert people_vaccinated >= people_fully_vaccinated
-    total_vaccinations = people_vaccinated + people_fully_vaccinated + booster_shots
+        date = soup.find(class_="reportdate").text
+        date = re.search(r"\d+ \w+ 202\d", date).group(0)
+        date = str(pd.to_datetime(date).date())
 
-    date = soup.find(class_="reportdate").text
-    date = re.search(r"\d+ \w+ 202\d", date).group(0)
-    date = str(pd.to_datetime(date).date())
+        data = {
+            "total_vaccinations": total_vaccinations,
+            "people_vaccinated": people_vaccinated,
+            "people_fully_vaccinated": people_fully_vaccinated,
+            "total_boosters": booster_shots,
+            "date": date,
+        }
+        return pd.Series(data=data)
 
-    data = {
-        "total_vaccinations": total_vaccinations,
-        "people_vaccinated": people_vaccinated,
-        "people_fully_vaccinated": people_fully_vaccinated,
-        "date": date,
-    }
-    return pd.Series(data=data)
+    def pipe_location(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "location", self.location)
 
+    def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(
+            ds,
+            "vaccine",
+            "Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing, Sputnik V",
+        )
 
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Bahrain")
+    def pipe_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "source_url", self.source_url)
 
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return (
+            ds.pipe(self.pipe_location)
+            .pipe(self.pipe_vaccine)
+            .pipe(self.pipe_source)[
+                [
+                    "location",
+                    "date",
+                    "vaccine",
+                    "source_url",
+                    "total_vaccinations",
+                    "people_vaccinated",
+                    "people_fully_vaccinated",
+                    "total_boosters",
+                ]
+            ]
+        )
 
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(
-        ds,
-        "vaccine",
-        "Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing, Sputnik V",
-    )
-
-
-def enrich_source(ds: pd.Series, source: str) -> pd.Series:
-    return enrich_data(ds, "source_url", source)
-
-
-def pipeline(ds: pd.Series, source: str) -> pd.Series:
-    return ds.pipe(enrich_location).pipe(enrich_vaccine).pipe(enrich_source, source)
+    def export(self):
+        data = self.read().pipe(self.pipeline)
+        increment(
+            location=data["location"],
+            total_vaccinations=data["total_vaccinations"],
+            people_vaccinated=data["people_vaccinated"],
+            people_fully_vaccinated=data["people_fully_vaccinated"],
+            total_boosters=data["total_boosters"],
+            date=data["date"],
+            source_url=data["source_url"],
+            vaccine=data["vaccine"],
+        )
 
 
 def main():
-    source = "https://healthalert.gov.bh/en/"
-    data = read(source).pipe(pipeline, source)
-    increment(
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"],
-    )
+    Bahrain().export()
 
 
 if __name__ == "__main__":
