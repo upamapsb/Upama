f4529f65600ac16cc97fb913b2914e6d0d9842a8
Edouard Mathieu <edmat@pm.me>
2021-12-23 16:10:38 +0100

    feat(hosp,finland): collect from national source

--- scripts/src/cowidev/hosp/sources/ecdc.py
+++ scripts/src/cowidev/hosp/sources/ecdc.py
@@ -14,6 +14,7 @@ EXCLUDED_COUNTRIES = [
     "Belgium",
     "Czechia",
     "Denmark",
+    "Finland",
     "France",
     "Germany",
     "Netherlands",
---
+++ scripts/src/cowidev/hosp/sources/finland.py
@@ -0,0 +1,37 @@
+import requests
+
+import pandas as pd
+
+SOURCE_URL = "https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaHospitalData"
+
+
+def main() -> pd.DataFrame:
+
+    print("Downloading Finland data…")
+
+    data = requests.get(SOURCE_URL).json()
+    df = pd.DataFrame.from_records(data["hospitalised"])
+
+    df = df[df.area == "Finland"][["date", "totalHospitalised", "inIcu"]]
+    import pdb
+
+    pdb.set_trace()
+    df["date"] = df.date.astype(str).str.slice(0, 10)
+
+    df = df.melt("date", var_name="indicator").dropna(subset=["value"])
+    df["indicator"] = df.indicator.replace(
+        {
+            "totalHospitalised": "Daily hospital occupancy",
+            "inIcu": "Daily ICU occupancy",
+        }
+    )
+
+    df["entity"] = "Finland"
+    df["iso_code"] = "FIN"
+    df["population"] = 5548361
+
+    return df
+
+
+if __name__ == "__main__":
+    main()
