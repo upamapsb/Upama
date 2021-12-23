3c820539efdf2e4438ab71042069a6fc555d9264
Edouard Mathieu <edmat@pm.me>
2021-12-23 14:36:22 +0100

    enhance(hosp, spain): add 3-day censoring

--- scripts/src/cowidev/hosp/sources/spain.py
+++ scripts/src/cowidev/hosp/sources/spain.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 
 SOURCE_URL = "https://cnecovid.isciii.es/covid19/resources/casos_hosp_uci_def_sexo_edad_provres.csv"
@@ -9,6 +11,7 @@ def main() -> pd.DataFrame:
     df = pd.read_csv(SOURCE_URL, usecols=["fecha", "num_hosp", "num_uci"])
 
     df = df.rename(columns={"fecha": "date"}).groupby("date", as_index=False).sum().sort_values("date")
+    df = df[pd.to_datetime(df.date).dt.date < (datetime.date.today() - datetime.timedelta(days=3))]
 
     df["num_hosp"] = df.num_hosp.rolling(7).sum()
     df["num_uci"] = df.num_uci.rolling(7).sum()
