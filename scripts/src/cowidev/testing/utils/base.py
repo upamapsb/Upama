b315a38bb5622fdd3dca439213348325727d3b24
lucas rg <lucasrg@kth.se>
2021-12-28 16:00:29 +0100

    feat(tests,utils): export specific filename

--- scripts/src/cowidev/testing/utils/base.py
+++ scripts/src/cowidev/testing/utils/base.py
@@ -3,6 +3,8 @@ from cowidev.utils import paths
 
 
 class CountryTestBase:
-    def export_datafile(self, df):
-        output_path = os.path.join(paths.SCRIPTS.OLD, "testing", "automated_sheets", f"{self.location}.csv")
+    def export_datafile(self, df, filename=None):
+        if filename is None:
+            filename = self.location
+        output_path = os.path.join(paths.SCRIPTS.OLD, "testing", "automated_sheets", f"{filename}.csv")
         df.to_csv(output_path, index=False)
