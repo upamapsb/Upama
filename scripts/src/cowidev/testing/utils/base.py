ca5ab7f86228e088886d38c43530eb0619e13e94
lucas rg <lucasrg@kth.se>
2021-12-28 16:49:54 +0100

    enhance(test,utils): CountryTestBase

--- scripts/src/cowidev/testing/utils/base.py
+++ scripts/src/cowidev/testing/utils/base.py
@@ -6,13 +6,20 @@ from cowidev.utils import paths
 
 
 class CountryTestBase:
-    def export_datafile(self, df, filename=None, attach=False):
+    def output_path(self, filename=None):
         if filename is None:
             filename = self.location
         output_path = os.path.join(paths.SCRIPTS.OLD, "testing", "automated_sheets", f"{filename}.csv")
+        return output_path
+
+    def export_datafile(self, df, filename=None, attach=False):
+        output_path = self.output_path(filename)
         if attach:
             df = merge_with_current_data(df, output_path)
         df.to_csv(output_path, index=False)
+
+    def load_datafile(self, filename=None):
+        return pd.read_csv(self.output_path(filename))
 
 
 def merge_with_current_data(df: pd.DataFrame, filepath: str) -> pd.DataFrame:
