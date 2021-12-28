a376d264a249d162ea0384ac5aa87381251e3bf3
lucas rg <lucasrg@kth.se>
2021-12-28 16:49:01 +0100

    refactor(test,elsalvador): OO

--- scripts/scripts/testing/automations/batch/el_salvador.py
+++ scripts/scripts/testing/automations/batch/el_salvador.py
@@ -1,52 +1,56 @@
 import datetime
-import requests
 
-from bs4 import BeautifulSoup
 import pandas as pd
-import numpy as np
 
-data = pd.read_csv("automated_sheets/El Salvador.csv")
+from cowidev.testing import CountryTestBase
+from cowidev.utils import get_soup, clean_count
+from cowidev.utils.clean.dates import clean_date, localdate
 
-date = pd.to_datetime(data.Date.max()).date()
-enddate = datetime.date.today()
 
-while date < enddate:
+class ElSalvador(CountryTestBase):
+    location: str = "El Salvador"
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
-    }
-    source_url = (
-        f"https://diario.innovacion.gob.sv/?fechaMostrar={date.strftime('%d-%m-%Y')}"
-    )
+    def read(self):
+        df = self.load_datafile()
+        date = clean_date(df.Date.max(), "%Y-%m-%d", as_datetime=True)
+        end_date = localdate(None, force_today=True, as_datetime=True)
+        records = []
+        while date < end_date:
+            print(date)
+            source_url = f"https://diario.innovacion.gob.sv/?fechaMostrar={date.strftime('%d-%m-%Y')}"
+            soup = get_soup(source_url)
+            daily = clean_count(
+                soup.find("div", class_="col-4 col-sm-2 col-lg-2 align-self-center offset-lg-0")
+                .find("label")
+                .text.strip()
+            )
+            records.append(
+                {
+                    "Date": [clean_date(date, "%Y-%m-%d")],
+                    "Daily change in cumulative total": daily,
+                }
+            )
+            # increment
+            date += datetime.timedelta(days=1)
 
-    try:
-        req = requests.get(source_url, headers=headers, timeout=10)
-    except Exception as e:
-        req = requests.get(source_url, headers=headers, timeout=10)
+        # Build dataframe
+        df_new = pd.DataFrame.from_records(records)
+        df_new = df_new.assign(
+            {
+                "Country": self.location,
+                "Units": "tests performed",
+                "Source label": "Government of El Salvador",
+                "Source URL": source_url,
+            }
+        )
+        df = pd.concat([df, df_new])
+        df = df.drop_duplicates().dropna(subset=["Daily change in cumulative total"])
+        self.export_datafile(df)
 
-    soup = BeautifulSoup(req.content, "html.parser")
 
-    daily = pd.to_numeric(
-        soup.find("div", class_="col-4 col-sm-2 col-lg-2 align-self-center offset-lg-0")
-        .find("label")
-        .text.strip()
-    )
+def main():
+    ElSalvador().read()
 
-    new = pd.DataFrame(
-        {
-            "Date": [str(date)],
-            "Daily change in cumulative total": daily,
-            "Country": "El Salvador",
-            "Units": "tests performed",
-            "Source label": "Government of El Salvador",
-            "Source URL": source_url,
-        }
-    )
 
-    # print(f"{date} - {daily}")
-
-    data = pd.concat([data, new])
-    data = data[data["Daily change in cumulative total"].fillna(0) > 0]
-    data.drop_duplicates().to_csv("automated_sheets/El Salvador.csv", index=False)
-
-    date += datetime.timedelta(days=1)
+if __name__ == "__main__":
+    main()
