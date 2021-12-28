974631db53e405112059f05fedb4b15ee1533451
lucas rg <lucasrg@kth.se>
2021-12-28 16:20:28 +0100

    feat(test,utils): export merging with existing file

--- scripts/src/cowidev/testing/utils/base.py
+++ scripts/src/cowidev/testing/utils/base.py
@@ -1,10 +1,22 @@
 import os
+
+import pandas as pd
+
 from cowidev.utils import paths
 
 
 class CountryTestBase:
-    def export_datafile(self, df, filename=None):
+    def export_datafile(self, df, filename=None, attach=False):
         if filename is None:
             filename = self.location
         output_path = os.path.join(paths.SCRIPTS.OLD, "testing", "automated_sheets", f"{filename}.csv")
+        if attach:
+            df = merge_with_current_data(df, output_path)
         df.to_csv(output_path, index=False)
+
+
+def merge_with_current_data(df: pd.DataFrame, filepath: str) -> pd.DataFrame:
+    df_current = pd.read_csv(filepath)
+    df_current = df_current[df_current.Date < df.Date.min()]
+    df = pd.concat([df_current, df]).sort_values("Date")
+    return df
