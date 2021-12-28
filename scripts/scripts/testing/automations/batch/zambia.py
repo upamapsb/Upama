c572bb40466bd216f4205922c6f493c827b7a39f
lucas rg <lucasrg@kth.se>
2021-12-28 15:01:57 +0100

    refactor(test,zambia)

--- scripts/scripts/testing/automations/batch/zambia.py
+++ scripts/scripts/testing/automations/batch/zambia.py
@@ -2,11 +2,12 @@
 ArcGIS Dashboard: https://zambia-open-data-nsdi-mlnr.hub.arcgis.com/pages/zambia-covid19
 """
 
-import json
 import datetime
-import requests
 import pandas as pd
-from utils import make_monotonic
+
+from cowidev.testing import CountryTestBase
+from cowidev.testing.utils import make_monotonic
+from cowidev.utils.web import request_json
 
 COUNTRY = "Zambia"
 UNITS = "tests performed"
@@ -28,53 +29,57 @@ PARAMS = {
 }
 
 
-def main() -> None:
-    df = get_data()
-    df = df.sort_values("Date")
-    df["Country"] = COUNTRY
-    df["Units"] = UNITS
-    df["Source URL"] = SOURCE_URL
-    df["Source label"] = SOURCE_LABEL
-    df["Notes"] = pd.NA
-    df = df[
-        [
-            "Country",
-            "Units",
-            "Date",
-            "Cumulative total",
-            "Source URL",
-            "Source label",
-            "Notes",
+class Zambia(CountryTestBase):
+    location: str = "Zambia"
+
+    def get_data(self) -> pd.DataFrame:
+        json_data = request_json(DATA_URL, params=PARAMS)
+        df = pd.DataFrame([feat["attributes"] for feat in json_data["features"]])
+        df["reportdt"] = df["reportdt"].astype(int).apply(lambda dt: datetime.datetime.utcfromtimestamp(dt / 1000))
+        df = df.rename(columns={"totalTests": "Cumulative total"})
+        df["Cumulative total"] = df["Cumulative total"].astype(int)
+        # KLUDGE: there are a few days with two reports on the same day (but at
+        # different times, like 10am vs 10pm). Upon inspection, it appears that the
+        # latter reports (e.g. the 10pm reports) actually correspond to official cumulative
+        # totals for the subsequent day (as determined by comparing to official updates
+        # published on Twitter and Facebook). So I increment the date of these latter
+        # reports by one.
+        df = df.sort_values("reportdt")
+        duplicate_idx = df.index[df["reportdt"].dt.date.duplicated(keep="first")]
+        for idx in duplicate_idx:
+            df.loc[idx, "reportdt"] = df.loc[idx, "reportdt"] + datetime.timedelta(days=1)
+        df["Date"] = df["reportdt"].dt.strftime("%Y-%m-%d")
+        df = df[["Date", "Cumulative total"]]
+        df = df[df["Cumulative total"] > 0]
+        df = df.groupby("Cumulative total", as_index=False).min()
+        df = df.groupby("Date", as_index=False).min()
+        df = make_monotonic(df)
+        return df
+
+    def export(self) -> None:
+        df = self.get_data()
+        df = df.sort_values("Date")
+        df["Country"] = COUNTRY
+        df["Units"] = UNITS
+        df["Source URL"] = SOURCE_URL
+        df["Source label"] = SOURCE_LABEL
+        df["Notes"] = pd.NA
+        df = df[
+            [
+                "Country",
+                "Units",
+                "Date",
+                "Cumulative total",
+                "Source URL",
+                "Source label",
+                "Notes",
+            ]
         ]
-    ]
-    df.to_csv("automated_sheets/Zambia.csv", index=False)
+        self.export_datafile(df)
 
 
-def get_data() -> pd.DataFrame:
-    res = requests.get(DATA_URL, params=PARAMS)
-    assert res.ok
-    json_data = json.loads(res.text)
-    df = pd.DataFrame([feat["attributes"] for feat in json_data["features"]])
-    df["reportdt"] = df["reportdt"].astype(int).apply(lambda dt: datetime.datetime.utcfromtimestamp(dt / 1000))
-    df = df.rename(columns={"totalTests": "Cumulative total"})
-    df["Cumulative total"] = df["Cumulative total"].astype(int)
-    # KLUDGE: there are a few days with two reports on the same day (but at
-    # different times, like 10am vs 10pm). Upon inspection, it appears that the
-    # latter reports (e.g. the 10pm reports) actually correspond to official cumulative
-    # totals for the subsequent day (as determined by comparing to official updates
-    # published on Twitter and Facebook). So I increment the date of these latter
-    # reports by one.
-    df = df.sort_values("reportdt")
-    duplicate_idx = df.index[df["reportdt"].dt.date.duplicated(keep="first")]
-    for idx in duplicate_idx:
-        df.loc[idx, "reportdt"] = df.loc[idx, "reportdt"] + datetime.timedelta(days=1)
-    df["Date"] = df["reportdt"].dt.strftime("%Y-%m-%d")
-    df = df[["Date", "Cumulative total"]]
-    df = df[df["Cumulative total"] > 0]
-    df = df.groupby("Cumulative total", as_index=False).min()
-    df = df.groupby("Date", as_index=False).min()
-    df = make_monotonic(df)
-    return df
+def main():
+    Zambia().export()
 
 
 if __name__ == "__main__":
