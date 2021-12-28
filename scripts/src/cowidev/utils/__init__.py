d680790c2cf61f9397ac698e521d60abbc544dee
lucas rg <lucasrg@kth.se>
2021-12-28 14:48:29 +0100

    refactor(utils): bring utils level up

---
+++ scripts/src/cowidev/utils/__init__.py
@@ -0,0 +1,10 @@
+from cowidev.utils.web import get_soup
+from cowidev.utils.clean import clean_date, clean_date_series, clean_count
+
+
+__all__ = [
+    "get_soup",
+    "clean_date",
+    "clean_date_series",
+    "clean_count",
+]
