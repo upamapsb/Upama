db459a0bcdb1b160484b34be96cc75ba3db49f01
lucas rg <lucasrg@kth.se>
2021-12-29 18:59:07 +0100

    refactor(test,ireland): new structure

--- scripts/scripts/testing/automations/batch/ireland.py
+++ scripts/scripts/testing/automations/batch/ireland.py
@@ -9,128 +9,64 @@ import requests
 import datetime
 import pandas as pd
 
-COUNTRY = "Ireland"
-UNITS = "tests performed"
-TESTING_TYPE = "PCR only"
-SOURCE_LABEL = "Government of Ireland"
-SOURCE_URL = "https://covid19ireland-geohive.hub.arcgis.com/pages/hospitals-icu--testing"
-
-SERIES_TYPE = "Cumulative total"  # one of: {'Cumulative total', 'Daily change in cumulative total'}
-DATE_COL = "Date_HPSC"
-DATA_URL = "https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/LaboratoryLocalTimeSeriesHistoricView/FeatureServer/0/query"
-PARAMS = {
-    "f": "json",
-    "where": f"{DATE_COL}>'2020-01-01 00:00:00'",  # "Dates>'2020-01-01 00:00:00'",
-    "returnGeometry": False,
-    "spatialRel": "esriSpatialRelIntersects",
-    "outFields": f"{DATE_COL},TotalLabs,Test24",
-    "orderByFields": f"{DATE_COL} asc",
-    "resultOffset": 0,
-    "resultRecordCount": 32000,
-    "resultType": "standard",
-}
-
-# hardcoded values
-hardcoded_data = [
-    # {'Date': "", SERIES_TYPE: , "Source URL": ""},
-]
+from cowidev.testing import CountryTestBase
 
 
-# sample of official values for cross-checking against the unofficial data.
-sample_official_data = [
-    (
-        "2020-03-18",
-        {
-            SERIES_TYPE: 6457,
-            "source": "https://covid19ireland-geohive.hub.arcgis.com/pages/hospitals-icu--testing",
-        },
-    ),
-    (
-        "2020-03-21",
-        {
-            SERIES_TYPE: 10436,
-            "source": "https://covid19ireland-geohive.hub.arcgis.com/pages/hospitals-icu--testing",
-        },
-    ),
-    (
-        "2020-04-09",
-        {
-            SERIES_TYPE: 58506,
-            "source": "https://covid19ireland-geohive.hub.arcgis.com/pages/hospitals-icu--testing",
-        },
-    ),
-    (
-        "2020-04-30",
-        {
-            SERIES_TYPE: 169646,
-            "source": "https://covid19ireland-geohive.hub.arcgis.com/pages/hospitals-icu--testing",
-        },
-    ),
-    (
-        "2020-05-16",
-        {
-            SERIES_TYPE: 282185,
-            "source": "https://covid19ireland-geohive.hub.arcgis.com/pages/hospitals-icu--testing",
-        },
-    ),
-]
+class Ireland(CountryTestBase):
+    location = "Ireland"
+    units = "tests performed"
+    TESTING_TYPE = "PCR only"
+    source_label = "Government of Ireland"
+    source_url_ref = "https://covid19ireland-geohive.hub.arcgis.com/pages/hospitals-icu--testing"
+    source_url = "https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/LaboratoryLocalTimeSeriesHistoricView/FeatureServer/0/query"
+    rename_columns = {
+        "Date_HPSC": "Date",
+        "Test24": "Daily change in cumulative total",
+        "TotalLabs": "Cumulative total",
+    }
 
+    def read(self):
+        DATE_COL = "Date_HPSC"
+        params = {
+            "f": "json",
+            "where": f"{DATE_COL}>'2020-01-01 00:00:00'",  # "Dates>'2020-01-01 00:00:00'",
+            "returnGeometry": False,
+            "spatialRel": "esriSpatialRelIntersects",
+            "outFields": f"{DATE_COL},TotalLabs,Test24",
+            "orderByFields": f"{DATE_COL} asc",
+            "resultOffset": 0,
+            "resultRecordCount": 32000,
+            "resultType": "standard",
+        }
+        res = requests.get(self.source_url, params=params)
+        json_data = json.loads(res.text)
+        df = pd.DataFrame([d["attributes"] for d in json_data["features"]])
+        return df
 
-def main() -> None:
-    df = get_data()
-    df["Source URL"] = df["Source URL"].apply(lambda x: SOURCE_URL if pd.isnull(x) else x)
-    df["Country"] = COUNTRY
-    df["Units"] = UNITS
-    df["Source label"] = SOURCE_LABEL
-    df["Notes"] = ""
-    sanity_checks(df)
-    df = df[
-        [
-            "Country",
-            "Units",
-            "Date",
-            SERIES_TYPE,
-            "Source URL",
-            "Source label",
-            "Notes",
-        ]
-    ]
-    df.to_csv("automated_sheets/Ireland.csv", index=False)
-    return None
+    def pipe_date(self, df: pd.DataFrame):
+        df["Date"] = df["Date"].astype(int).apply(lambda dt: datetime.datetime.utcfromtimestamp(dt / 1000))
+        df["Date"] = df["Date"].dt.strftime("%Y-%m-%d")
+        return df
 
+    def pipeline(self, df: pd.DataFrame):
+        df = df.pipe(self.pipe_rename_columns)
+        df = df.pipe(self.pipe_date)
+        # drops duplicate YYYY-MM-DD rows.
+        # df[df[DATE_COL].dt.strftime('%Y-%m-%d').duplicated(keep=False)]  # prints out rows with duplicate YYYY-MM-DD value
+        # df.sort_values(DATE_COL, inplace=True)
+        # df.drop_duplicates(subset=['Date'], keep='last', inplace=True)
 
-def get_data() -> pd.DataFrame:
-    res = requests.get(DATA_URL, params=PARAMS)
-    json_data = json.loads(res.text)
-    df = pd.DataFrame([d["attributes"] for d in json_data["features"]])
-    df[DATE_COL] = df[DATE_COL].astype(int).apply(lambda dt: datetime.datetime.utcfromtimestamp(dt / 1000))
-    df["Date"] = df[DATE_COL].dt.strftime("%Y-%m-%d")
-    # drops duplicate YYYY-MM-DD rows.
-    # df[df[DATE_COL].dt.strftime('%Y-%m-%d').duplicated(keep=False)]  # prints out rows with duplicate YYYY-MM-DD value
-    # df.sort_values(DATE_COL, inplace=True)
-    # df.drop_duplicates(subset=['Date'], keep='last', inplace=True)
-    df.rename(
-        columns={
-            "Test24": "Daily change in cumulative total",
-            "TotalLabs": "Cumulative total",
-        },
-        inplace=True,
-    )
-    df["Source URL"] = None
-    df = df[["Date", SERIES_TYPE, "Source URL"]]
-    if len(hardcoded_data) > 0:
-        # removes rows from df that are hardcoded
-        hardcoded_dates = [d["Date"] for d in hardcoded_data]
-        df = df[~df["Date"].isin(hardcoded_dates)]
-        # appends hardcoded rows to df
-        df_hardcoded = pd.DataFrame(hardcoded_data)
-        df = pd.concat([df, df_hardcoded], axis=0, sort=False).reset_index(drop=True)
-    df.sort_values("Date", inplace=True)
-    df.dropna(subset=["Date", SERIES_TYPE], how="any", inplace=True)
-    df[SERIES_TYPE] = df[SERIES_TYPE].astype(int)
-    # with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.max_colwidth', None):
-    #     print(df)
-    return df
+        df = df[["Date", "Cumulative total"]]
+        df = df.sort_values("Date").dropna(subset=["Date", "Cumulative total"], how="any")
+        df["Cumulative total"] = df["Cumulative total"].astype(int)
+        df = df.pipe(self.pipe_metadata)
+        return df
+
+    def export(self) -> None:
+        df = self.read().pipe(self.pipeline)
+        sanity_checks(df)
+        self.export_datafile(df)
+        return None
 
 
 def sanity_checks(df: pd.DataFrame) -> None:
@@ -148,13 +84,11 @@ def sanity_checks(df: pd.DataFrame) -> None:
     assert (
         df_temp["Cumulative total"].iloc[1:] >= df_temp["Cumulative total"].shift(1).iloc[1:]
     ).all(), "On one or more dates, `Cumulative total` is greater on date t-1."
-    # df.iloc[1:][df['Cumulative total'].iloc[1:] < df['Cumulative total'].shift(1).iloc[1:]]
-    # cross-checks a sample of scraped figures against the expected result.
-    # assert len(sample_official_data) > 0
-    # for dt, d in sample_official_data:
-    #     val = df_temp.loc[df_temp['Date'] == dt, SERIES_TYPE].squeeze().sum()
-    #     assert val == d[SERIES_TYPE], f"scraped value ({val:,d}) != official value ({d[SERIES_TYPE]:,d}) on {dt}"
     return None
+
+
+def main():
+    Ireland().export()
 
 
 if __name__ == "__main__":
