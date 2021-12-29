f4da901a3c613255a8876a6cf25201df25b88135
lucas rg <lucasrg@kth.se>
2021-12-29 18:59:19 +0100

    refactor(test,israel): new structure

--- scripts/scripts/testing/automations/batch/israel.py
+++ scripts/scripts/testing/automations/batch/israel.py
@@ -2,42 +2,36 @@ import pandas as pd
 
 
 from cowidev.testing import CountryTestBase
+from cowidev.utils import clean_date_series
 
 
 class Israel(CountryTestBase):
-    location: str = "Israel"
-    source_url: str = "https://datadashboardapi.health.gov.il/api/queries/testResultsPerDate"
+    location = "Israel"
+    source_url = "https://datadashboardapi.health.gov.il/api/queries/testResultsPerDate"
+    units = "tests performed"
+    source_label = "Israel Ministry of Health"
+    source_url_ref = source_url
+    rename_columns = {
+        "date": "Date",
+        "amount": "Daily change in cumulative total",
+    }
 
     def read(self):
         df = pd.read_json(self.source_url)[["date", "amount"]]
         return df
 
     def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = df.rename(
-            columns={
-                "date": "Date",
-                "amount": "Daily change in cumulative total",
-            }
-        )
-        df = df.assign(
-            **{
-                "Date": df.Date.dt.strftime("%Y-%m-%d"),
-                "Country": self.location,
-                "Units": "tests performed",
-                "Source label": "Israel Ministry of Health",
-                "Source URL": self.source_url,
-                "Notes": pd.NA,
-            }
-        )
+        df = df.pipe(self.pipe_rename_columns).pipe(self.pipe_metadata)
+        df = df.assign(Date=clean_date_series(df.Date))
         return df
 
-    def to_csv(self):
+    def export(self):
         df = self.read().pipe(self.pipeline)
         self.export_datafile(df)
 
 
 def main():
-    Israel().to_csv()
+    Israel().export()
 
 
 if __name__ == "__main__":
