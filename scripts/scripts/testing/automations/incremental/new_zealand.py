c13dd305a69e240e3886ccd52568eabebd5c4fe6
lucasrodes <lucasrodes@users.noreply.github.com>
2021-12-30 16:30:03 +0100

    refactor(test,nz): read function

--- scripts/scripts/testing/automations/incremental/new_zealand.py
+++ scripts/scripts/testing/automations/incremental/new_zealand.py
@@ -7,25 +7,32 @@ from cowidev.testing.utils.incremental import increment
 
 
 class NewZealand:
-    location: str = "New Zealand"
-    units: str = "tests performed"
-    source_label: str = "Ministry of Health"
-    source_url: str = "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-data-and-statistics/covid-19-testing-data"
-    notes: str = ""
+    location = "New Zealand"
+    units = "tests performed"
+    source_label = "Ministry of Health"
+    source_url = "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-data-and-statistics/covid-19-testing-data"
+    notes = ""
 
-    def _parse_data(self):
-        soup = get_soup(self.source_url)
-        quests = requests.get(self.source_url)
-        table = pd.read_html(quests.text, index_col=0)[1]
-
-        date_raw = soup.select_one(".pane-content .georgia-italic").text
+    def read(self):
         return {
-            "count": int(table.loc["All COVID-19 viral tests administered*"]),
-            "date": extract_clean_date(date_raw, regex="(\\d+ \\w+ 202\\d)", date_format="%d %B %Y"),
+            "count": self._parse_metric(),
+            "date": self._parse_date(),
         }
 
+    def _parse_metric(self):
+        quests = requests.get(self.source_url)
+        table = pd.read_html(quests.text, index_col=0)[1]
+        count = int(table.loc["All COVID-19 viral tests administered*"])
+        return count
+
+    def _parse_date(self):
+        soup = get_soup(self.source_url)
+        date_raw = soup.select_one(".pane-content .georgia-italic").text
+        dt_raw = extract_clean_date(date_raw, regex="(\\d+ \\w+ 202\\d)", date_format="%d %B %Y")
+        return dt_raw
+
     def export(self):
-        data = self._parse_data()
+        data = self.read()
         increment(
             count=data["count"],
             sheet_name=self.location,
