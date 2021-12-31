6ff384c6736c7dc53f9039ee43953b8d040dc074
lucasrodes <lucasrodes@users.noreply.github.com>
2021-12-31 19:08:59 +0100

    refactor(test,turkey): new structure

--- scripts/scripts/testing/automations/batch/turkey.py
+++ scripts/scripts/testing/automations/batch/turkey.py
@@ -1,29 +1,37 @@
-import datetime
-from selenium import webdriver
 import pandas as pd
 import dateparser
+from cowidev.testing import CountryTestBase
+from cowidev.utils.web.scraping import get_driver
 
 
-SOURCE_URL = "https://covid19.saglik.gov.tr/TR-66935/genel-koronavirus-tablosu.html"
+class Turkey(CountryTestBase):
+    location = "Turkey"
+    source_url = "https://covid19.saglik.gov.tr/TR-66935/genel-koronavirus-tablosu.html"
+    source_url_ref = source_url
+    units = "tests performed"
+    source_label = "Turkish Ministry of Health"
+    rename_columns = {
+        "Tarih": "Date",
+        "Toplam Test Sayısı": "Cumulative total",
+        "Bugünkü Test Sayısı": "Daily change in cumulative total",
+    }
 
+    def read(self):
+        table = self._parse_table()
+        df = pd.read_html(table, thousands=".")[0]
+        return df
 
-def main():
+    def _parse_table(self):
+        with get_driver() as driver:
+            # with webdriver.Chrome() as driver:
+            driver.get(self.source_url)
+            table = driver.find_element_by_class_name("table-striped")
+            return table.get_attribute("outerHTML")
 
-    with webdriver.Firefox() as driver:
-        driver.get(SOURCE_URL)
+    def pipe_filter(self, df: pd.DataFrame):
+        return df[["Tarih", "Bugünkü Test Sayısı"]].dropna()
 
-        table = driver.find_element_by_class_name("table-striped")
-        df = pd.read_html(table.get_attribute("outerHTML"), thousands=".")[0]
-        df = df[["Tarih", "Bugünkü Test Sayısı"]].dropna()
-
-        df = df.rename(
-            columns={
-                "Tarih": "Date",
-                "Toplam Test Sayısı": "Cumulative total",
-                "Bugünkü Test Sayısı": "Daily change in cumulative total",
-            }
-        )
-
+    def pipe_date(self, df: pd.DataFrame):
         df["Date"] = (
             df["Date"]
             .str.replace("MAYIS", "mayıs")
@@ -31,13 +39,19 @@ def main():
             .str.replace("ARALIK", "aralık")
             .apply(dateparser.parse, languages=["tr"])
         )
-        df.loc[:, "Country"] = "Turkey"
-        df.loc[:, "Units"] = "tests performed"
-        df.loc[:, "Source URL"] = SOURCE_URL
-        df.loc[:, "Source label"] = "Turkish Ministry of Health"
-        df.loc[:, "Notes"] = pd.NA
+        return df
 
-        df.to_csv("automated_sheets/Turkey.csv", index=False)
+    def pipeline(self, df: pd.DataFrame):
+        df = df.pipe(self.pipe_filter).pipe(self.pipe_rename_columns).pipe(self.pipe_date).pipe(self.pipe_metadata)
+        return df
+
+    def export(self):
+        df = self.read().pipe(self.pipeline)
+        self.export_datafile(df)
+
+
+def main():
+    Turkey().export()
 
 
 if __name__ == "__main__":
