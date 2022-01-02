bdcd716a194276ca36563f5a1c6f22192c19ceb4
lucasrodes <lucasrodes@users.noreply.github.com>
2022-01-02 12:38:46 +0100

    fix(vax,cuba): date generation

--- scripts/src/cowidev/vax/incremental/cuba.py
+++ scripts/src/cowidev/vax/incremental/cuba.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 import pandas as pd
 
-from cowidev.utils.clean import clean_count, clean_date
-from cowidev.utils.web.scraping import get_soup
+from cowidev.utils import clean_count, clean_date, get_soup
+from cowidev.utils.clean.dates import localdate
 from cowidev.vax.utils.incremental import increment, enrich_data
 
 
@@ -22,20 +22,32 @@ class Cuba:
 
     def read(self) -> pd.Series:
         soup = get_soup(self.source_url)
-        return self.parse_data(soup)
+        return self._parse_data(soup)
 
-    def parse_data(self, soup: BeautifulSoup) -> pd.Series:
-        data = {}
+    def _parse_data(self, soup: BeautifulSoup) -> pd.Series:
+        return pd.Series(
+            data={
+                "date": self._parse_date(soup),
+                **self._parse_metrics(soup),
+            }
+        )
 
+    def _parse_date(self, soup):
         match = re.search(self.regex["title"], soup.text)
         date_str = match.group(1).replace("ro", "")
-        data["date"] = clean_date(f"{date_str} {datetime.now().year}", "%d de %B %Y", lang="es")
-        data["total_vaccinations"] = clean_count(match.group(2))
+        date = clean_date(f"{date_str} {datetime.now().year}", "%d de %B %Y", lang="es")
+        if date > localdate("America/Havana", force_today=True):
+            date = clean_date(f"{date_str} {datetime.now().year-1}", "%d de %B %Y", lang="es")
+        return date
+
+    def _parse_metrics(self, soup):
+        match = re.search(self.regex["title"], soup.text)
+        data = {"total_vaccinations": clean_count(match.group(2))}
 
         for metric in ["people_vaccinated", "people_fully_vaccinated", "total_boosters"]:
             match = re.search(self.regex[metric], soup.text)
             data[metric] = clean_count(match.group(1))
-        return pd.Series(data)
+        return data
 
     def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
         return enrich_data(ds, "vaccine", "Abdala, Soberana02")
