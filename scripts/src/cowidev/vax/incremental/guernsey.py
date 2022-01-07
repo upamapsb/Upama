9d09fba6ca43d7cc975ccfb09f34fc6f50c95315
lucasrodes <lucasrodes@users.noreply.github.com>
2022-01-07 18:05:13 +0100

    data(vax,guernsey): fix total_vaccinations

--- scripts/src/cowidev/vax/incremental/guernsey.py
+++ scripts/src/cowidev/vax/incremental/guernsey.py
@@ -6,19 +6,21 @@ from cowidev.vax.utils.incremental import enrich_data, increment
 
 
 class Guernsey:
-    def __init__(self, source_url: str, location: str):
-        self.source_url = source_url
-        self.location = location
-        self._regex_date = r"This page was last updated on (\d{1,2} [A-Za-z]+ 202\d)"
+    source_url = "https://covid19.gov.gg/guidance/vaccine/stats"
+    location = "Guernsey"
+    _regex_date = r"This page was last updated on (\d{1,2} [A-Za-z]+ 202\d)"
 
     def read(self) -> pd.Series:
         soup = get_soup(self.source_url)
-        return self.parse_data(soup)
+        df = self.parse_data(soup)
+        print(df)
+        return df
 
     def parse_data(self, soup):
         # Get table
         tables = soup.find_all("table")
         ds = pd.read_html(str(tables[0]))[0].squeeze()
+        print(ds.loc[ds[0] == "Total doses", 1].values[0])
         # Rename, add/remove columns
         return pd.Series(
             {
@@ -26,7 +28,7 @@ class Guernsey:
                     text=str(soup.text), regex=self._regex_date, date_format="%d %B %Y", lang="en"
                 ),
                 "total_vaccinations": clean_count(
-                    ds.loc[ds[0] == "Total doses", 1].values[0],
+                    ds.loc[ds[0] == "Total doses", 1].values[0].replace("*", ""),
                 ),
             }
         )
@@ -43,7 +45,7 @@ class Guernsey:
     def pipeline(self, ds: pd.Series) -> pd.Series:
         return ds.pipe(self.pipe_location).pipe(self.pipe_vaccine).pipe(self.pipe_source)
 
-    def to_csv(self):
+    def export(self):
         """Generalized."""
         data = self.read().pipe(self.pipeline)
         increment(
@@ -56,10 +58,7 @@ class Guernsey:
 
 
 def main():
-    Guernsey(
-        source_url="https://covid19.gov.gg/guidance/vaccine/stats",
-        location="Guernsey",
-    ).to_csv()
+    Guernsey().export()
 
 
 if __name__ == "__main__":
