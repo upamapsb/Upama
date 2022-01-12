fc4edda83da25fa897ef3b984b1e30e7d6655915
lucasrodes <lucasrodes@users.noreply.github.com>
2022-01-12 14:04:14 +0100

    refactor(vax,slovakia): change source Institut-Zdravotnych-Analyz/covid19-data#148

--- scripts/src/cowidev/vax/batch/slovakia.py
+++ scripts/src/cowidev/vax/batch/slovakia.py
@@ -1,104 +1,104 @@
+from datetime import datetime, timedelta
+
 import pandas as pd
 
-from cowidev.utils import paths
+from cowidev.utils import clean_date, paths
+from cowidev.utils.clean.dates import localdatenow
 from cowidev.utils.utils import check_known_columns
 from cowidev.vax.utils.checks import VACCINES_ONE_DOSE
 from cowidev.vax.utils.utils import build_vaccine_timeline
 
 
 class Slovakia:
-    location: str = "Slovakia"
-    source_url: str = (
+    location = "Slovakia"
+    source_url = (
         "https://github.com/Institut-Zdravotnych-Analyz/covid19-data/raw/main/Vaccination/"
-        "OpenData_Slovakia_Vaccination_Regions.csv"
+        "OpenData_Slovakia_Vaccination_AgeGroup_District.csv"
     )
-    source_url_ref: str = "https://github.com/Institut-Zdravotnych-Analyz/covid19-data"
-    vaccine_rename: dict = {
-        "Comirnaty koncentrát na injekčnú disperziu": "Pfizer/BioNTech",
-        "Comirnaty 10 mikrogramov/dávka koncentrát na injekčnú disperziu, con dsi 10x1,3 ml (liek.inj.skl.)": (
-            "Pfizer/BioNTech"
-        ),
-        "Spikevax injekčná disperzia (pôvodne COVID-19 Vaccine Moderna)dis inj 10x5 ml (liek.inj.skl.)": "Moderna",
-        "Vaxzevria injekčná suspenzia sus inj 10x5 ml (liek.inj.skl.) (pôvodne COVID-19 Vaccine AstraZeneca)": (
-            "Oxford/AstraZeneca"
-        ),
-        "COVID-19 Vaccine Janssen injekčná suspenzia sus inj 10x2,5 ml (liek.inj.skl.)": "Johnson&Johnson",
-        "COVID-19 Vaccine Janssen injekčná suspenzia sus inj 20x2,5 ml (liek.inj.skl.)": "Johnson&Johnson",
-        "Sputnik V(Gam-COVID-Vac) lag orig. 5x3 ml, komponenta I-rekombinantný ľudský adenovírus sérotypu 26,komponenta I-rekombinantný ľ": (
-            "Sputnik V"
-        ),
-        "Vaxzevria injekčná suspenzia sus inj 10x4 ml (liek.inj.skl.) (pôvodne COVID-19 Vaccine AstraZeneca)": (
-            "Oxford/AstraZeneca"
-        ),
+    source_url_ref = "https://github.com/Institut-Zdravotnych-Analyz/covid19-data"
+    vaccine_mapping = {
+        "ASTRAZENECA": "Oxford/AstraZeneca",
+        "COMIRNATY": "Pfizer/BioNTech",
+        "JANSSEN": "Johnson&Johnson",
+        "MODERNA": "Moderna",
+        "SPUTNIK": "Sputnik V",
     }
-    column_rename: dict = {"Date": "date"}
+    vax_timeline = None
+    date_start = datetime(2021, 1, 4)
 
     def read(self):
         df = pd.read_csv(self.source_url, sep=";")
         check_known_columns(
-            df, ["Date", "Region", "Region_code", "Vaccine_name", "first_dose", "second_dose", "third_dose"]
+            df,
+            [
+                "week",
+                "vaccine",
+                "gender",
+                "AgeGroup",
+                "region",
+                "district",
+                "district_code",
+                "dose",
+                "doses_administered",
+            ],
         )
         return df
 
-    def pipe_vaccine_rename(self, df: pd.DataFrame) -> pd.DataFrame:
-        vax_wrong = set(df["Vaccine_name"]).difference(self.vaccine_rename.keys())
+    def pipe_date(self, df: pd.DataFrame) -> pd.DataFrame:
+        # Change week to date
+
+        return df.assign(date=df.week.apply(self._week_to_date))
+
+    def _week_to_date(self, week):
+        date = clean_date(self.date_start + timedelta(weeks=week))
+        if date > localdatenow("Europe/Bratislava"):
+            return localdatenow("Europe/Bratislava")
+        return date
+
+    def pipe_vaccine_checks(self, df: pd.DataFrame) -> pd.DataFrame:
+        # Get vax timeline
+        vax_wrong = set(df.vaccine).difference(self.vaccine_mapping)
         if vax_wrong:
-            raise ValueError(
-                f"Unknown vaccines detected {vax_wrong}! Please,  review class attribute self.vaccine_rename."
-            )
-        df = df.assign(vaccine=df["Vaccine_name"].map(self.vaccine_rename))
+            raise ValueError(f"Unknown vaccine(s): {vax_wrong}")
+        df = df.assign(vaccine=df.vaccine.replace(self.vaccine_mapping))
+        self.vax_timeline = df.groupby("vaccine").date.min().to_dict()
+        # Check one dose vax
+        vax_1d = [vax for vax in self.vaccine_mapping.values() if vax in VACCINES_ONE_DOSE]
+        if "2" in set(df.loc[df.vaccine.isin(vax_1d), "dose"]):
+            raise ValueError("Some single-dose vaccines are registering second doses!")
         return df
 
-    def pipe_rename_columns(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df.rename(columns=self.column_rename)
+    def pipe_reshape(self, df: pd.DataFrame) -> pd.DataFrame:
+        # Group
+        df = df.groupby(["date", "dose"], as_index=False).doses_administered.sum()
+        # Pivot
+        df = df.pivot(index=["date"], columns="dose", values="doses_administered").reset_index()
+        return df
+
+    def pipe_cumsum(self, df: pd.DataFrame) -> pd.DataFrame:
+        # Cummulative
+        df = df.sort_values("date")
+        cols = ["1", "2", "fully", "3"]
+        df[cols] = df[cols].cumsum().fillna(method="ffill").fillna(0)
+        return df
 
     def pipe_metrics(self, df: pd.DataFrame) -> pd.DataFrame:
-        single_dose = df.first_dose.copy()
-        single_dose.loc[~df.vaccine.isin(VACCINES_ONE_DOSE)] = 0
-        df = df.assign(
-            total_vaccinations=df.first_dose + df.second_dose + df.third_dose,
-            people_vaccinated=df.first_dose,
-            people_fully_vaccinated=df.second_dose + single_dose,
-            total_boosters=df.third_dose,
+        # Metrics
+        return df.assign(
+            total_vaccinations=df["1"] + df["2"] + df["3"],
+            people_vaccinated=df["1"],
+            people_fully_vaccinated=df.fully,
+            total_boosters=df["3"],
         )
-        return df
 
-    def pipe_metrics_aggregate(self, df: pd.DataFrame) -> pd.DataFrame:
-        vax_timeline = df.groupby("vaccine").date.min().to_dict()
-        df = (
-            df.groupby("date", as_index=False)
-            .agg(
-                {
-                    "total_vaccinations": "sum",
-                    "people_vaccinated": "sum",
-                    "people_fully_vaccinated": "sum",
-                    "total_boosters": "sum",
-                }
-            )
-            .sort_values("date")  # change to descending
-        )
+    def pipe_metadata(self, df: pd.DataFrame) -> pd.DataFrame:
+        # Metadata
+        df = df.assign(location=self.location, source_url=self.source_url)
         # Add vaccines
         return build_vaccine_timeline(
             df,
-            vax_timeline,
+            self.vax_timeline,
         )
-
-    def pipe_metrics_cumsum(self, df: pd.DataFrame) -> pd.DataFrame:
-        df["people_vaccinated"] = df["people_vaccinated"].cumsum()
-        df["people_fully_vaccinated"] = df["people_fully_vaccinated"].cumsum()
-        df["total_boosters"] = df["total_boosters"].cumsum()
-        df["total_vaccinations"] = df["total_vaccinations"].cumsum()
-        return df
-
-    def pipe_vaccine(self, df: pd.DataFrame) -> pd.DataFrame:
-        df.loc[:, "vaccine"] = "Pfizer/BioNTech"
-        df.loc[df.date >= "2021-01-27", "vaccine"] = "Moderna, Pfizer/BioNTech"
-        df.loc[df.date >= "2021-02-13", "vaccine"] = "Moderna, Oxford/AstraZeneca, Pfizer/BioNTech, Sputnik V"
-        return df
-
-    def pipe_metadata(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = df.assign(location=self.location, source_url=self.source_url_ref)
-        return df
 
     def pipe_out_columns(self, df: pd.DataFrame) -> pd.DataFrame:
         return df[
@@ -116,11 +116,11 @@ class Slovakia:
 
     def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
         return (
-            df.pipe(self.pipe_vaccine_rename)
-            .pipe(self.pipe_rename_columns)
+            df.pipe(self.pipe_date)
+            .pipe(self.pipe_vaccine_checks)
+            .pipe(self.pipe_reshape)
+            .pipe(self.pipe_cumsum)
             .pipe(self.pipe_metrics)
-            .pipe(self.pipe_metrics_aggregate)
-            .pipe(self.pipe_metrics_cumsum)
             # .pipe(self.pipe_vaccine)
             .pipe(self.pipe_metadata)
             .pipe(self.pipe_out_columns)
