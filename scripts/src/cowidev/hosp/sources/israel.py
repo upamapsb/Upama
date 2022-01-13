20f8a8e5bb67b5aff283e6c892d2eb089278509b
Edouard Mathieu <edmat@pm.me>
2022-01-13 10:56:15 +0100

    fix(vax,israel): switch source & metrics

--- scripts/src/cowidev/hosp/sources/israel.py
+++ scripts/src/cowidev/hosp/sources/israel.py
@@ -2,35 +2,39 @@ import pandas as pd
 
 
 METADATA = {
-    "source_url": "https://raw.githubusercontent.com/dancarmoz/israel_moh_covid_dashboard_data/master/hospitalized_and_infected.csv",
+    "source_url_stock": "https://github.com/erasta/CovidDataIsrael/raw/master/out/csv/hospitalizationStatusDaily.csv",
+    "source_url_flow": "https://github.com/erasta/CovidDataIsrael/raw/master/out/csv/patientsPerDate.csv",
     "source_url_ref": "https://datadashboard.health.gov.il/COVID-19/",
-    "source_name": "Ministry of Health, via dancarmoz on GitHub",
+    "source_name": "Ministry of Health, via Eran Geva on GitHub",
     "entity": "Israel",
 }
 
 
 def main():
+
+    stock = pd.read_csv(METADATA["source_url_stock"]).rename(columns={"dayDate": "date"})
+    stock["hospital_stock"] = stock.countHardStatus + stock.countMediumStatus + stock.countEasyStatus
+    stock = stock.drop(columns=["countMediumStatus", "countEasyStatus"])
+
+    flow = pd.read_csv(METADATA["source_url_flow"], usecols=["date", "serious_critical_new"]).sort_values("date")
+    flow["serious_critical_new"] = flow.serious_critical_new.rolling(7).sum()
+
     df = (
-        pd.read_csv(
-            METADATA["source_url"], usecols=["Date", "Hospitalized", "Critical", "New hosptialized", "New serious"]
-        )
-        .rename(columns={"Date": "date"})
+        pd.merge(stock, flow, on="date", how="outer", validate="one_to_one")
+        .melt("date", var_name="indicator")
+        .dropna(subset=["value"])
         .sort_values("date")
+        .head(-1)
     )
-
-    df["New hosptialized"] = df["New hosptialized"].rolling(7).sum()
-    df["New serious"] = df["New serious"].rolling(7).sum()
-
-    df = df.melt("date", var_name="indicator").dropna(subset=["value"])
     df["indicator"] = df.indicator.replace(
         {
-            "Hospitalized": "Daily hospital occupancy",
-            "Critical": "Daily ICU occupancy",
-            "New hosptialized": "Weekly new hospital admissions",
-            "New serious": "Weekly new ICU admissions",
+            "hospital_stock": "Daily hospital occupancy",
+            "countHardStatus": "Daily ICU occupancy",
+            "serious_critical_new": "Weekly new ICU admissions",
         }
     )
 
+    df["date"] = df.date.str.slice(0, 10)
     df["entity"] = METADATA["entity"]
 
     return df, METADATA
