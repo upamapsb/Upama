9bd37323636794fb3eaded72132aeff917481e76
Edouard Mathieu <edmat@pm.me>
2022-01-15 11:22:23 +0100

    enhance(testing): auto collection commit message

--- scripts/scripts/testing/auto_quick_collect.sh
+++ scripts/scripts/testing/auto_quick_collect.sh
@@ -4,5 +4,5 @@ cd ~/git/covid-19-data/scripts/scripts/testing
 source venv/bin/activate
 bash collect_data.sh quick
 git add automated_sheets/*
-git commit -m "Automated testing collection"
+git commit -m "data(testing): automated collection"
 git push
